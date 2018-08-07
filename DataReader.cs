1e4ae16972bad6c5cebf780791bb099f83c5a461
Leon <leopard10000@gmail.com>
2018-08-07 14:19:23 +0900

    Mujin Programming Challenge 2018: C問題: RubyでTLEだった実装をC#に移植し、564msecでAC

---
+++ DataReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace AtCoderCS
+{
+	public class DataReader : IReader
+	{
+		TextReader _reader;
+		public DataReader(TextReader reader) {
+			_reader = reader;
+		}
+
+		public string ReadLine() {
+			return _reader.ReadLine();
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -3,10 +3,49 @@ using System.Linq;
 
 namespace AtCoderCS
 {
-    class MainClass
-    {
-		public static void Main(string[] args)
-		{
+	public interface IReader
+	{
+		string ReadLine();
+	}
+
+	public class ConsoleReader : IReader
+	{
+		public string ReadLine() {
+			return Console.ReadLine();
+		}
+	}
+
+	public class SPoint
+	{
+		public SPoint(uint left) {
+			Left = left;
+		}
+		public uint Left { get; set; }
+		public uint Right { get; set; }
+		public uint Upper { get; set; }
+		public uint Lower { get; set; }
+		public override string ToString() {
+			return $"[LRUD=({Left},{Right},{Upper},{Lower})]";
+		}
+
+		public uint PermuteCount() {
+			return (Left + Right) * (Upper + Lower);
+		}
+	}
+
+	public class Solver
+	{
+		IReader _reader;
+
+		public Solver(IReader reader = null) {
+			if (reader == null) {
+				_reader = new ConsoleReader();
+			} else {
+				_reader = reader;
+			}
+		}
+
+		public void Run() {
 			//// 整数の入力
 			//int a = int.Parse(Console.ReadLine());
 			//// スペース区切りの整数の入力
@@ -18,12 +57,84 @@ namespace AtCoderCS
 			////出力
 			//Console.WriteLine((a + b + c) + " " + s);
 
-			var nums = Console.ReadLine().Split().Select(int.Parse).ToList();
+			var nums = _reader.ReadLine().Split().Select(int.Parse).ToArray();
+			var height = nums[0];
+			var width = nums[1];
+			//Console.WriteLine($"{height}, {width}");
 
-			int max = nums.Max();
-			int min = nums.Min();
+			SPoint[,] grid = new SPoint[height, width];
+			for (int r = 0; r < height; r++) {
+				uint s = 0;
+				var line = _reader.ReadLine();
+				for (int c = 0; c < width; c++) {
+					if (line[c] == '#') {
+						s = 0;
+					} else {
+						grid[r, c] = new SPoint(s);
+						++s;
+					}
+				}
 
-			Console.WriteLine(max - min);
+				// 右からのスペース数をカウントアップ
+				s = 0;
+				for (int c = width - 1; 0 <= c; c--) {
+					if (grid[r, c] == null)
+						s = 0;
+					else {
+						grid[r, c].Right += s;
+						++s;
+					}
+				}
+
+				// 上からのスペースをカウントアップ
+				if (0 < r) {
+					for (int c = 0; c < width; c++) {
+						if (grid[r, c] != null && grid[r - 1, c] != null) {
+							grid[r, c].Upper = grid[r - 1, c].Upper + 1;
+						}
+					}
+				}
+			}
+
+			for (int r = height - 1; 0 <= r; r--) {
+				if (r < height - 1) {
+					// 下からのスペースをカウントアップ
+					for (int c = 0; c < width; c++) {
+						if (grid[r, c] != null && grid[r + 1, c] != null) {
+							grid[r, c].Lower = grid[r + 1, c].Lower + 1;
+						}
+					}
+				}
+			}
+			//DumpGrid(height, width, grid);
+
+			// 順序対をカウントアップ
+			ulong ans = 0;
+			foreach (var sp in grid) {
+				if (sp != null) {
+					ans += sp.PermuteCount();
+				}
+			}
+			Console.WriteLine(ans);
 		}
-    }
+
+		private static void DumpGrid(int height, int width, SPoint[,] grid) {
+			for (int r = 0; r < height; r++) {
+				for (int c = 0; c < width; c++) {
+					if (grid[r, c] == null)
+						Console.Write("#, ");
+					else
+						Console.Write($"{grid[r, c]}, ");
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+
+	class MainClass
+	{
+		public static void Main(string[] args) {
+			new Solver().Run();
+		}
+	}
 }
