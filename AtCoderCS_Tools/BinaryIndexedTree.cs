4fb2649a43a2ed91c50aa8a26fd1b8f22aac1acf
Leon <leopard10000@gmail.com>
2019-01-03 08:59:11 +0900

    何も変更していないんだけど？？？

---
+++ AtCoderCS_Tools/BinaryIndexedTree.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace AtCoderCS_Tools
+{
+	public class BinaryIndexedTree
+	{
+		public int MaxNum { get; private set; }
+
+		int[] bit;
+
+		public BinaryIndexedTree(int maxNum) {
+			MaxNum = maxNum;
+			bit = new int[maxNum + 1];
+		}
+
+		// i の個数を x 増加
+		// ※i は1以上
+		public void Add(int i, int v) {
+			for (int x = i; x <= MaxNum; x += x & -x)
+				bit[x] += v;
+		}
+
+		// i 以下の総数を返す
+		// ※0以下のi を与えた場合は0を返す
+		public int Sum(int i) {
+			int ret = 0;
+			for (int x = i; 0 < x; x -= x & -x)
+				ret += bit[x];
+			return ret;
+		}
+
+		// [lower, upper] つまり lower以上 upper以下 の総数を返す
+		public int RangeSum(int lower, int upper) {
+			return Sum(upper) - Sum(lower-1);
+		}
+
+		// i の個数を返す
+		public int Count(int i) {
+			return Sum(i) - Sum(i - 1);
+		}
+	}
+}
---
+++ AtCoderCS_Tools_NUnit/BinaryIndexedTree_Test.cs
@@ -0,0 +1,52 @@
+﻿using AtCoderCS_Tools;
+using NUnit.Framework;
+using System;
+namespace AtCoderCS_Tools_NUnit
+{
+	[TestFixture()]
+	public class BinaryIndexedTreeTest
+	{
+		[Test()]
+		public void Init() {
+			var bit = new BinaryIndexedTree(10);
+
+			Assert.AreEqual(10, bit.MaxNum);
+			Assert.AreEqual(0, bit.Sum(0));
+			Assert.AreEqual(0, bit.Sum(1));
+			Assert.AreEqual(0, bit.Sum(10));
+			//Assert.AreEqual(0, bit.Sum(11));
+
+			Assert.AreEqual(0, bit.RangeSum(2, 4));
+
+			Assert.AreEqual(0, bit.Count(2));
+		}
+
+		[Test]
+		public void Operation() {
+			var bit = new BinaryIndexedTree(10);
+			Assert.AreEqual(0, bit.Sum(10));
+
+			bit.Add(2, 1);
+			Assert.AreEqual(0, bit.Sum(1));
+			Assert.AreEqual(1, bit.Sum(2));
+			bit.Add(5, 3);
+			Assert.AreEqual(1, bit.Sum(4));
+			Assert.AreEqual(4, bit.Sum(5));
+			bit.Add(10, 10);
+			Assert.AreEqual(4, bit.Sum(9));
+			Assert.AreEqual(14, bit.Sum(10));
+			bit.Add(9, -100);
+			Assert.AreEqual(4, bit.Sum(8));
+			Assert.AreEqual(-96, bit.Sum(9));
+			Assert.AreEqual(-86, bit.Sum(10));
+
+			Assert.AreEqual(-97, bit.RangeSum(3, 9));
+			Assert.AreEqual(-87, bit.RangeSum(5, 10));
+
+			Assert.AreEqual(3, bit.Count(5));
+			Assert.AreEqual(0, bit.Count(6));
+			Assert.AreEqual(-100, bit.Count(9));
+			Assert.AreEqual(10, bit.Count(10));
+		}
+	}
+}
