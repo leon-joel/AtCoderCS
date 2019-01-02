c14f8d759cc7bb00188ce71690d857c65121f5f8
Leon <leopard10000@gmail.com>
2019-01-02 21:53:05 +0900

    ナップサックDP: 部分和問題: DPとBFSで実装
    https://qiita.com/drken/items/ace3142967c4f01d42e9#%E8%A7%A3%E6%B3%95-3-%E5%80%8B%E6%95%B0%E5%88%B6%E9%99%90%E3%81%AA%E3%81%97%E3%83%8A%E3%83%83%E3%83%97%E3%82%B5%E3%83%83%E3%82%AF-dp

---
+++ AtCoderCS_NUnit/KnapsackProblem/Test_PartialSumProblems.cs
@@ -0,0 +1,99 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using KnapsackProblem;
+
+namespace PartialSumProblems
+{
+	public class NunitSolver : Solver
+	{
+		readonly StringReader _reader;
+		readonly StringBuilder _sb;
+		public NunitSolver(string input, StringBuilder writer) {
+			_reader = new StringReader(input);
+			_sb = writer;
+		}
+		override protected string ReadLine() => _reader.ReadLine();
+		override protected int ReadInt() => int.Parse(_reader.ReadLine());
+		override protected long ReadLong() => long.Parse(_reader.ReadLine());
+		override protected string[] ReadStringArray() => _reader.ReadLine().Split(' ');
+		override protected int[] ReadIntArray() => _reader.ReadLine().Split(' ').Select<string, int>(s => int.Parse(s)).ToArray();
+		override protected long[] ReadLongArray() => _reader.ReadLine().Split(' ').Select<string, long>(s => long.Parse(s)).ToArray();
+		override protected void WriteLine(string line) => _sb.AppendLine(line);
+		override protected void WriteLine<T>(T value) =>_sb.AppendLine(value.ToString());
+	}
+	[TestFixture()]
+	public class SolverTest {
+		[TestCaseSource(typeof(TestDataFactory), "Cases")]
+		public void TestCase(TestData data) {
+			var sb = new StringBuilder();
+			var solver = new NunitSolver(data.Input, sb);
+			var sw = Stopwatch.StartNew();
+			solver.Run();
+			sw.Stop();
+			Console.WriteLine($"Elapsed: {sw.Elapsed}");
+			Assert.AreEqual(data.Expected, sb.ToString().TrimEnd());
+		}
+	}
+
+	public class TestData {
+		public string Name { get; private set; }
+		public string Input { get; private set; }
+		public string Expected { get; private set; }
+
+		public override string ToString() => Name;
+
+		public TestData(string name, string input, string expected) {
+			Name = name;
+			Input = input;
+			Expected = expected.Replace("\r\n", "\n");
+		}
+	}
+	public static class TestDataFactory {
+		public static TestData[] Cases() {
+			return new TestData[] {
+				new TestData("Test1",
+@"3 10
+7 5 3",
+@"Yes"),
+				new TestData("Test2",
+@"2 6
+9 7",
+@"No"),
+				new TestData("Test3",
+@"3 10
+7 4 8",
+@"No"),
+				new TestData("Test4",
+@"3 10
+7 10 3",
+@"Yes"),
+//				new TestData("Test5",
+//@"37",
+//@"2"),
+//				new TestData("Test6",
+//@"35",
+//@"6"),
+//				new TestData("Test7",
+//@"36",
+//@"1"),
+//				new TestData("Test8",
+//@"81",
+//@"1"),
+//				new TestData("Test9",
+//@"13122",
+//@"2"),
+//				new TestData("Test10",
+//@"13123",
+//@"3"),
+//				new TestData("Test11",
+//@"93312",
+//@"2"),
+			};
+		}
+	}
+}
---
+++ KnapsackProblem/PartialSumProblems.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace PartialSumProblems
+{
+	// DP練習 〜 部分和問題とその応用たち
+	// https://qiita.com/drken/items/a5e6fe22863b7992efdb#3-%E9%83%A8%E5%88%86%E5%92%8C%E5%95%8F%E9%A1%8C%E3%81%A8%E3%81%9D%E3%81%AE%E5%BF%9C%E7%94%A8%E3%81%9F%E3%81%A1
+	public class Solver
+	{
+		virtual protected string ReadLine() => Console.ReadLine();
+		virtual protected int ReadInt() => int.Parse(ReadLine());
+		virtual protected long ReadLong() => long.Parse(ReadLine());
+		virtual protected string[] ReadStringArray() => ReadLine().Split(' ');
+		virtual protected int[] ReadIntArray() => ReadLine().Split(' ').Select<string, int>(s => int.Parse(s)).ToArray();
+		virtual protected long[] ReadLongArray() => ReadLine().Split(' ').Select<string, long>(s => long.Parse(s)).ToArray();
+		virtual protected void WriteLine(string line) => Console.WriteLine(line);
+		virtual protected void WriteLine<T>(T value) where T : IFormattable => Console.WriteLine(value);
+
+		[Conditional("DEBUG")]
+		void Dump<T>(IEnumerable<T> array) {
+			var sb = new StringBuilder();
+			foreach (var item in array) {
+				sb.Append(item);
+				sb.Append(", ");
+			}
+			// Consoleに出力すると、UnitTestの邪魔をしないというメリットあり。
+			Console.WriteLine(sb.ToString());
+			//_writer.WriteLine(sb.ToString());
+		}
+
+		int N;
+		int UpperW;
+		int[] Nums;
+
+		public void Run() {
+			var ary = ReadIntArray();
+			N = ary[0];
+			UpperW = ary[1];
+
+			Nums = ReadIntArray();
+
+			var ans = DpSolve();
+			//var ans = Bfs();
+			WriteLine(ans ? "Yes" : "No");
+		}
+
+		// 動的計画法（DP）（＝漸化式＋ループ）での実装
+		bool DpSolve() {
+			var DP = new bool[N + 1, UpperW + 1];
+			// boolのデフォルト値はfalseなので明示的な初期化不要
+			//for (int i = 0; i < N + 1; i++) {
+			//	for (int j = 0; j < UpperW + 1; j++) {
+			//		DP[i, j] = false;
+			//	}
+			//}
+			DP[0, 0] = true;
+
+			for (int i = 0; i < N; i++) {
+				for (int w = 0; w < UpperW + 1; w++) {
+					if (w < Nums[i]) {
+						// 現在itemの重さより左側（現在itemが使えない部分）は上から下ろしてくるだけ
+						DP[i + 1, w] = DP[i, w];
+					} else {
+						// 現在itemの重さ以上の部分（右側）は
+						// 重さ分左のvalue | 現在value or 下ろしてくるだけ
+						DP[i + 1, w] = DP[i, w] | DP[i, w - Nums[i]];
+					}
+				}
+			}
+
+			return DP[N, UpperW];
+		}
+
+		// 幅優先探索
+		// ※queueに格納される個数が多くなりすぎないか心配
+		bool Bfs() {
+			var DP = new bool[UpperW + 1];
+			DP[0] = true;
+
+			var que = new Queue<int>();
+			que.Enqueue(0);
+
+			// BFS
+			while (0 < que.Count) {
+				// queueに入っている＝配布可能
+				var v = que.Dequeue();
+
+				foreach (var w in Nums) {
+					if (v + w <= UpperW && !DP[v + w]) {
+						// 配布先に初めて配る＝キューに入れる
+						que.Enqueue(v + w);
+						DP[v + w] = true;
+					}
+				}
+			}
+
+			return DP[UpperW];
+		}
+
+#if !MYHOME
+		public static void Main(string[] args) {
+			new Solver().Run();
+		}
+#endif
+	}
+}
