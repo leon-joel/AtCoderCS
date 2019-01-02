6272a7b5323685f2cffbe581b387aa2e3ed172d6
Leon <leopard10000@gmail.com>
2019-01-02 11:18:25 +0900

    ナップサックDP: ABC099-C: 個数制限なしナップサックDP
    https://atcoder.jp/contests/abc099/tasks/abc099_c
    https://qiita.com/drken/items/ace3142967c4f01d42e9#%E8%A7%A3%E6%B3%95-3-%E5%80%8B%E6%95%B0%E5%88%B6%E9%99%90%E3%81%AA%E3%81%97%E3%83%8A%E3%83%83%E3%83%97%E3%82%B5%E3%83%83%E3%82%AF-dp

--- KnapsackProblem/ABC099C_StrangeBank.cs
+++ KnapsackProblem/ABC099C_StrangeBank.cs
@@ -32,42 +32,18 @@ namespace ABC099C_StrangeBank
 			//_writer.WriteLine(sb.ToString());
 		}
 
-		struct Item
-		{
-			public readonly int Weight;
-			public readonly int Value;
-
-			public Item(int v, int w) {
-				Value = v;
-				Weight = w;
-			}
-			public Item(int[] ary) {
-				Value = ary[0];
-				Weight = ary[1];
-			}
-			public override string ToString() {
-				return string.Format($"V:{Value} W:{Weight}");
-			}
-		}
-
 		int N;
 		int UpperW;
-		Item[] Items;
+
+		readonly int[] Sixes = { 1, 6, 36, 216, 1296, 7776, 46656 };
+		readonly int[] Nines = { 1, 9, 81, 729, 6561, 59049 };
+		readonly int[] Items = { 1, 6, 36, 216, 1296, 7776, 46656, 9, 81, 729, 6561, 59049 };
 
 		public void Run() {
-			var ary = ReadIntArray();
-			N = ary[0];
-			UpperW = ary[1];
+			UpperW = ReadInt();
+			N = Items.Length;
 
-			Items = new Item[N];
-			for (int i = 0; i < N; i++) {
-				var item = new Item(ReadIntArray());
-				//Console.WriteLine(item.ToString());
-				Items[i] = item;
-			}
-			//Dump(items);
-
-			// 動的計画法（DP）で解く
+			// 個数制限なしナップサックDPで解く
 			var ans = DpSolve();
 			// メモ探索で解く
 			//var ans = MemoizeRecursive();
@@ -76,28 +52,37 @@ namespace ABC099C_StrangeBank
 			WriteLine(ans);
 		}
 
-		// 動的計画法（DP）（＝漸化式＋ループ）での実装
-		// ※この実装はDP表を上から埋めていくが、多分下から埋めても同じこと。
-		//   https://www.slideshare.net/iwiwi/ss-3578511
+		// 個数制限なしナップサックDP
 		int[,] DP;
 		int DpSolve() {
 			DP = new int[N + 1, UpperW + 1];
 			for (int i = 0; i < N + 1; i++) {
 				for (int j = 0; j < UpperW + 1; j++) {
-					DP[i, j] = 0;
+					DP[i, j] = 1 << 29;	// INF
 				}
+			}
+			// 左端は全部0に
+			for (int i = 0; i < N + 1; i++) {
+				DP[i, 0] = 0;
 			}
 
 			for (int i = 0; i < N; i++) {
-				var item = Items[i];
 				for (int w = 0; w < UpperW + 1; w++) {
-					if (w < item.Weight) {
-						// 現在itemの重さより左側（現在itemが使えない部分）は上から下ろしてくるだけ
-						DP[i + 1, w] = DP[i, w];
+					if (w < Items[i]) {
+						// 現在の重さより左側は現在のアイテム（金額）を使えないので
+						// 上から下ろす or 既に有る値
+						DP[i + 1, w] = Math.Min(DP[i, w], DP[i + 1, w]);
 					} else {
-						// 現在itemの重さ以上の部分（右側）は
-						// 重さ分左のvalue + 現在value or 下ろしてくるだけ
-						DP[i + 1, w] = Math.Max(DP[i, w - item.Weight] + item.Value, DP[i, w]);
+						// 現在の重さ以上の部分（右側）は
+						// 重さ分左のvalue + 現在value or 下ろすだけ
+						DP[i + 1, w] = Math.Min(DP[i + 1, w - Items[i]] + 1, DP[i, w]);
+						//引き出し金額が 1円, 6円, 9円 の順にDPした場合の例
+						//※空欄はINF
+						//  w->0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18...
+						//0    0                                             ...
+						//1    0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18...
+						//6    0 1 2 3 4 5 1 2 3 4  5  6  2  3  4  5  6  7  3...
+						//9    0 1 2 3 4 5 1 2 3 1  2  3  2  3  4  2  3  4  2...
 					}
 				}
 			}
@@ -105,58 +90,6 @@ namespace ABC099C_StrangeBank
 			return DP[N, UpperW];
 		}
 
-		// メモ探索（＝再帰関数のメモ化）での実装
-		// https://www.slideshare.net/iwiwi/ss-3578511
-		int[,] Memo;
-		int MemoizeRecursive() {
-			Memo = new int[N, UpperW + 1];
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < UpperW + 1; j++) {
-					Memo[i, j] = -1;
-				}
-			}
-			return Search(0, UpperW);
-		}
-		int Search(int i, int upper) {
-			if (N <= i) return 0;
-
-			// 既に計算済みの場合はそれを返す
-			if (Memo[i, upper] != -1)
-				return Memo[i, upper];
-
-			var item = Items[i];
-
-			// もうこの品物は入らない
-			if (upper < item.Weight)
-				return Search(i + 1, upper);
-
-			// この品物を入れない場合と入れる場合の両方で探索
-			var value1 = Search(i + 1, upper);
-			var value2 = Search(i + 1, upper - item.Weight) + item.Value;
-
-			var value = Math.Max(value1, value2);
-			Memo[i, upper] = value;
-			return value;
-		}
-
-		// 全探索で解く実装
-		// https://www.slideshare.net/iwiwi/ss-3578511
-		// これだと200品目でも終わらない O(2^n)
-		int SearchAll(int i, int upper) {
-			// 品物が残ってない
-			if (N <= i) return 0;
-
-			Item item = Items[i];
-
-			// もうこの品物は入らない
-			if (upper < item.Weight)
-				return SearchAll(i + 1, upper);
-
-			// この品物を入れない場合と入れる場合の両方で探索
-			var value1 = SearchAll(i + 1, upper);
-			var value2 = SearchAll(i + 1, upper - item.Weight) + item.Value;
-			return Math.Max(value1, value2);
-		}
 #if !MYHOME
 		public static void Main(string[] args) {
 			new Solver().Run();
