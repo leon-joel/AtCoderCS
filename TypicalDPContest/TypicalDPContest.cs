db858331d725f59ddb9e737702204f2b19c75ad0
Leon <leopard10000@gmail.com>
2019-01-05 00:09:43 +0900

    TypicalDPContest: B: ゲーム ※2次元DP （手番を考慮に入れて、残りカード0枚から逆にたどるDP）
    https://tdpc.contest.atcoder.jp/tasks/tdpc_game

--- TypicalDPContest/TypicalDPContest.cs
+++ TypicalDPContest/TypicalDPContest.cs
@@ -6,8 +6,22 @@ using System.Text;
 
 namespace TypicalDPContest
 {
+	public static class Util
+	{
+		public static string DumpToString<T>(IEnumerable<T> array) where T : IFormattable {
+			var sb = new StringBuilder();
+			foreach (var item in array) {
+				sb.Append(item);
+				sb.Append(", ");
+			}
+			return sb.ToString();
+		}
+	}
+
 	// 典型的なDPコンテスト
 	// https://tdpc.contest.atcoder.jp/tasks/tdpc_contest
+	//
+	// A: コンテスト 
 	public class Solver
 	{
 		virtual protected string ReadLine() => Console.ReadLine();
@@ -99,6 +113,123 @@ namespace TypicalDPContest
 				if (DP[i]) ++cnt;
 			}
 			return cnt;
+		}
+
+#if !MYHOME
+		public static void Main(string[] args) {
+			new Solver().Run();
+		}
+#endif
+	}
+
+	// B: ゲーム 2次元DP
+	public class SolverB
+	{
+		virtual protected string ReadLine() => Console.ReadLine();
+		virtual protected int ReadInt() => int.Parse(ReadLine());
+		virtual protected long ReadLong() => long.Parse(ReadLine());
+		virtual protected string[] ReadStringArray() => ReadLine().Split(' ');
+		virtual protected int[] ReadIntArray() => ReadLine().Split(' ').Select<string, int>(s => int.Parse(s)).ToArray();
+		virtual protected long[] ReadLongArray() => ReadLine().Split(' ').Select<string, long>(s => long.Parse(s)).ToArray();
+		virtual protected void WriteLine(string line) => Console.WriteLine(line);
+		virtual protected void WriteLine<T>(T value) where T : IFormattable => Console.WriteLine(value);
+
+		[Conditional("DEBUG")]
+		void Dump<T>(IEnumerable<T> array) where T : IFormattable {
+			string s = Util.DumpToString(array);
+			// Consoleに出力すると、UnitTestの邪魔をしないというメリットあり。
+			Console.WriteLine(s);
+			//_writer.WriteLine(s);
+		}
+		[Conditional("DEBUG")]
+		void DumpGrid<T>(IEnumerable<IEnumerable<T>> arrayOfArray) where T : IFormattable {
+			var sb = new StringBuilder();
+			foreach (var a in arrayOfArray) {
+				sb.AppendLine(Util.DumpToString(a));
+			}
+			// Consoleに出力すると、UnitTestの邪魔をしないというメリットあり。
+			Console.WriteLine(sb.ToString());
+			//_writer.WriteLine(sb.ToString());
+		}
+		[Conditional("DEBUG")]
+		void DumpDP<T>(T[,] dp) where T : IFormattable {
+			var sb = new StringBuilder();
+			for (int i = 0; i < dp.GetLength(0); i++) {
+				for (int j = 0; j < dp.GetLength(1); j++) {
+					sb.Append(dp[i,j]);
+					sb.Append(", ");
+				}
+				sb.AppendLine();
+			}
+			Console.WriteLine(sb.ToString());
+		}
+
+		public void Run() {
+			var ary = ReadIntArray();
+			var nA = ary[0];
+			var nB = ary[1];
+
+			var numsA = ReadIntArray();
+			var numsB = ReadIntArray();
+
+			// 2次元DP
+			int[,] dp = new int[nA + 1, nB + 1];
+			// DP[A山の残り数, B山の残り数] = すぬけが取りうる最大の価値
+			// 初期化: DP[0, 0] = 0
+			//
+			// A山[1] B山[2, 10]の場合 ※先手は常にすぬけ君
+			// ※^^はすぬけ君が取った手。無印は相手の手。
+			//       10    2  ←B山
+			//   0 ← 10 ← 10
+			//   ↑   ^^   ↑
+			// 1 1 ←  1   11
+			//  ^^        ^^
+			// A山
+
+			// すぬけ君の手番 ※トータル数が奇数なら残数奇数が手番になる
+			int myTurn = (nA + nB) % 2;
+
+			// 左端を先に全部埋める ※余計なIndex境界判定を不要にできる
+			for (int i = 1; i < nA + 1; i++) {
+				if (i % 2 == myTurn) {
+					// すぬけ番なので上のに現在価値を加算して格納
+					dp[i, 0] = dp[i - 1, 0] + numsA[nA - i];
+				} else {
+					// 相手の番なので上のをそのまま格納
+					dp[i, 0] = dp[i - 1, 0];
+				}
+			}
+			// 同様に上端を全部埋める
+			for (int j = 1; j < nB + 1; j++) {
+				if (j % 2 == myTurn) {
+					// すぬけ番なので上のに現在価値を加算して格納
+					dp[0, j] = dp[0, j - 1] + numsB[nB - j];
+				} else {
+					// 相手の番なので上のをそのまま格納
+					dp[0, j] = dp[0, j - 1];
+				}
+			}
+			//DumpDP(dp);
+
+			// 貰うDP
+			for (int i = 1; i < nA + 1; i++) {
+				for (int j = 1; j < nB + 1; j++) {
+					if ((i + j) % 2 == myTurn) {
+						// すぬけ君の手番（＝価値を最大化する）
+						// 上＋A or 左＋B の大きい方を選択し、書き込み
+						var a = dp[i - 1, j] + numsA[nA - i];
+						var b = dp[i, j - 1] + numsB[nB - j];
+						dp[i, j] = Math.Max(a, b);
+					} else {
+						// 相手の手番（＝すぬけの価値を最小化する）
+						// 上 or 左 の小さい方を選択し、書き込み
+						dp[i, j] = Math.Min(dp[i - 1, j], dp[i, j - 1]);
+					}
+				}
+			}
+			//DumpDP(dp);
+
+			WriteLine(dp[nA, nB]);
 		}
 
 #if !MYHOME
