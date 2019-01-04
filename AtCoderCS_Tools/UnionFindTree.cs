62d71a83b237469ddfd23cbc4d93b6485da87966
Leon <leopard10000@gmail.com>
2019-01-04 10:37:47 +0900

    UnionFindTree をC#に移植

---
+++ AtCoderCS_Tools/UnionFindTree.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AtCoderCS_Tools
+{
+	class Node
+	{
+		public int Parent;
+		public int Rank;
+
+		public Node(int id) {
+			Parent = id;
+			Rank = 0;
+		}
+	}
+
+	public class UnionFindTree
+	{
+		readonly Node[] Nodes;
+
+		// 0 から n-1 まで（計 n個）のNodeを作る
+		public UnionFindTree(int n) {
+			Nodes = new Node[n];
+			for (int i = 0; i < n; i++) {
+				Nodes[i] = new Node(i);
+			}
+		}
+
+		// x要素のルート親を返す
+		public int Find(int x) {
+			if (Nodes[x].Parent == x)
+				return x;
+
+			return Nodes[x].Parent = Find(Nodes[x].Parent);
+		}
+
+		// a, b を同一グループ化する
+		// 既に同じグループだったらfalseを返す
+		public bool Unite(int a, int b) {
+			a = Find(a);
+			b = Find(b);
+
+			if (a == b)
+				return false;
+
+			if (Nodes[a].Rank < Nodes[b].Rank) {
+				Nodes[a].Parent = b;
+			} else {
+				Nodes[b].Parent = a;
+				if (Nodes[a].Rank == Nodes[b].Rank)
+					Nodes[a].Rank += 1;
+			}
+			return true;
+		}
+
+		// a, b が同一グループ？
+		public bool Same(int a, int b) {
+			return Find(a) == Find(b);
+		}
+	}
+}
---
+++ AtCoderCS_Tools_NUnit/UnionFindTree_Test.cs
@@ -0,0 +1,38 @@
+﻿using AtCoderCS_Tools;
+using NUnit.Framework;
+using System;
+namespace AtCoderCS_Tools_NUnit
+{
+	[TestFixture()]
+	public class UnionFindTreeTest
+	{
+		[Test()]
+		public void Init() {
+			var tree = new UnionFindTree(10);
+
+			Assert.AreEqual(0, tree.Find(0));
+		}
+
+		[Test]
+		public void Operation() {
+			var tree = new UnionFindTree(10);
+
+			Assert.AreEqual(0, tree.Find(0));
+			Assert.AreEqual(1, tree.Find(1));
+			Assert.IsTrue(tree.Unite(0, 1));
+			Assert.AreEqual(0, tree.Find(0));
+			Assert.AreEqual(0, tree.Find(1));
+			Assert.IsTrue(tree.Same(0, 1));
+
+			// 既に同一グループ
+			Assert.IsFalse(tree.Unite(0, 1));
+
+			// 別グループ
+			Assert.IsFalse(tree.Same(1, 2));
+
+			// 同一グループ化
+			Assert.IsTrue(tree.Unite(1, 2));
+			Assert.IsTrue(tree.Same(1, 2));
+		}
+	}
+}
