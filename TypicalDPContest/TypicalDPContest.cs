860c6f99a6121fb56937093a5b34d02de4b40201
Leon <leopard10000@gmail.com>
2019-01-05 21:23:00 +0900

    TypicalDPContest: C:トーナメント（bit演算による対戦相手の抽出） / D:サイコロ（多次元DP）
    https://tdpc.contest.atcoder.jp/tasks/tdpc_game

--- TypicalDPContest/TypicalDPContest.cs
+++ TypicalDPContest/TypicalDPContest.cs
@@ -4,6 +4,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
+// 典型的なDPコンテスト
+// https://tdpc.contest.atcoder.jp/tasks/tdpc_contest
+//
 namespace TypicalDPContest
 {
 	public static class Util
@@ -16,13 +19,17 @@ namespace TypicalDPContest
 			}
 			return sb.ToString();
 		}
+
+		public static void InitDP<T>(T[,] dp, T value) {
+			for (int i = 0; i < dp.GetLength(0); i++) {
+				for (int j = 0; j < dp.GetLength(1); j++) {
+					dp[i, j] = value;
+				}
+			}
+		}
 	}
 
-	// 典型的なDPコンテスト
-	// https://tdpc.contest.atcoder.jp/tasks/tdpc_contest
-	//
-	// A: コンテスト 
-	public class Solver
+	public class SolverBase
 	{
 		virtual protected string ReadLine() => Console.ReadLine();
 		virtual protected int ReadInt() => int.Parse(ReadLine());
@@ -32,19 +39,46 @@ namespace TypicalDPContest
 		virtual protected long[] ReadLongArray() => ReadLine().Split(' ').Select<string, long>(s => long.Parse(s)).ToArray();
 		virtual protected void WriteLine(string line) => Console.WriteLine(line);
 		virtual protected void WriteLine<T>(T value) where T : IFormattable => Console.WriteLine(value);
+		virtual protected void WriteLine(double d) => Console.WriteLine($"{d:F9}");
 
 		[Conditional("DEBUG")]
-		void Dump<T>(IEnumerable<T> array) {
+		protected void Dump(string s) => Console.WriteLine(s);
+		[Conditional("DEBUG")]
+		protected void Dump(double d) => Console.WriteLine($"{d:F9}");
+		[Conditional("DEBUG")]
+		protected void Dump<T>(IEnumerable<T> array) where T : IFormattable {
+			string s = Util.DumpToString(array);
+			// Consoleに出力すると、UnitTestの邪魔をしないというメリットあり。
+			Console.WriteLine(s);
+			//_writer.WriteLine(s);
+		}
+		[Conditional("DEBUG")]
+		protected void DumpGrid<T>(IEnumerable<IEnumerable<T>> arrayOfArray) where T : IFormattable {
 			var sb = new StringBuilder();
-			foreach (var item in array) {
-				sb.Append(item);
-				sb.Append(", ");
+			foreach (var a in arrayOfArray) {
+				sb.AppendLine(Util.DumpToString(a));
 			}
 			// Consoleに出力すると、UnitTestの邪魔をしないというメリットあり。
 			Console.WriteLine(sb.ToString());
 			//_writer.WriteLine(sb.ToString());
 		}
+		[Conditional("DEBUG")]
+		protected void DumpDP<T>(T[,] dp) where T : IFormattable {
+			var sb = new StringBuilder();
+			for (int i = 0; i < dp.GetLength(0); i++) {
+				for (int j = 0; j < dp.GetLength(1); j++) {
+					sb.Append(dp[i, j]);
+					sb.Append(", ");
+				}
+				sb.AppendLine();
+			}
+			Console.WriteLine(sb.ToString());
+		}
+	}
 
+	// A: コンテスト 
+	public class Solver : SolverBase
+	{
 		int N;
 		int[] Nums;
 		int UpperW = 100 * 100;
@@ -123,47 +157,8 @@ namespace TypicalDPContest
 	}
 
 	// B: ゲーム 2次元DP
-	public class SolverB
+	public class SolverB : SolverBase
 	{
-		virtual protected string ReadLine() => Console.ReadLine();
-		virtual protected int ReadInt() => int.Parse(ReadLine());
-		virtual protected long ReadLong() => long.Parse(ReadLine());
-		virtual protected string[] ReadStringArray() => ReadLine().Split(' ');
-		virtual protected int[] ReadIntArray() => ReadLine().Split(' ').Select<string, int>(s => int.Parse(s)).ToArray();
-		virtual protected long[] ReadLongArray() => ReadLine().Split(' ').Select<string, long>(s => long.Parse(s)).ToArray();
-		virtual protected void WriteLine(string line) => Console.WriteLine(line);
-		virtual protected void WriteLine<T>(T value) where T : IFormattable => Console.WriteLine(value);
-
-		[Conditional("DEBUG")]
-		void Dump<T>(IEnumerable<T> array) where T : IFormattable {
-			string s = Util.DumpToString(array);
-			// Consoleに出力すると、UnitTestの邪魔をしないというメリットあり。
-			Console.WriteLine(s);
-			//_writer.WriteLine(s);
-		}
-		[Conditional("DEBUG")]
-		void DumpGrid<T>(IEnumerable<IEnumerable<T>> arrayOfArray) where T : IFormattable {
-			var sb = new StringBuilder();
-			foreach (var a in arrayOfArray) {
-				sb.AppendLine(Util.DumpToString(a));
-			}
-			// Consoleに出力すると、UnitTestの邪魔をしないというメリットあり。
-			Console.WriteLine(sb.ToString());
-			//_writer.WriteLine(sb.ToString());
-		}
-		[Conditional("DEBUG")]
-		void DumpDP<T>(T[,] dp) where T : IFormattable {
-			var sb = new StringBuilder();
-			for (int i = 0; i < dp.GetLength(0); i++) {
-				for (int j = 0; j < dp.GetLength(1); j++) {
-					sb.Append(dp[i,j]);
-					sb.Append(", ");
-				}
-				sb.AppendLine();
-			}
-			Console.WriteLine(sb.ToString());
-		}
-
 		public void Run() {
 			var ary = ReadIntArray();
 			var nA = ary[0];
@@ -235,6 +230,180 @@ namespace TypicalDPContest
 #if !MYHOME
 		public static void Main(string[] args) {
 			new Solver().Run();
+		}
+#endif
+	}
+
+	// C: トーナメント
+	public class SolverC : SolverBase
+	{
+		public void Run() {
+			var K = ReadInt();
+			var playerNum = (int)Math.Pow(2, K);
+			var Rates = new int[playerNum];
+
+			for (int i = 0; i < playerNum; i++) {
+				Rates[i] = ReadInt();
+			}
+
+			// 動的計画法（DP）（＝漸化式＋ループ）での実装
+			// DP[i回戦, j番目の選手] = i回戦目まで勝ち上がってくる確率(~1)
+			var DP = new double[K+1, playerNum];
+			// 0回戦目は全員勝ち上がり確率 1 とする
+			for (int i = 0; i < playerNum; i++) {
+				DP[0, i] = 1;
+			}
+
+			for (int i = 1; i < K+1; i++) {
+				for (int j = 0; j < playerNum; j++) {
+					// i回戦目(1~)の対戦相手
+					//   jのiビット目を反転
+					//   その下位ビット（がもしあれば）をbit全探索
+					//Dump($"{i}回戦: {j}");
+
+					// 積算対戦勝率
+					double sumRatio = 0;
+					var prefix = j ^ (1 << (i - 1));	// iビットだけ反転
+					prefix &= int.MaxValue << (i - 1);	// iビットより下位は全部0クリ
+					for (int r = 0; r < (1 << (i - 1)); r++) {
+						// 対戦相手
+						var o = prefix | r;
+						//Dump(o.ToString());
+
+						// += 勝率 * 相手がそのラウンドまで勝ち上がってくる確率
+						sumRatio += CalcWinRatioOfA(Rates[j], Rates[o]) * DP[i-1, o];
+					}
+
+					// 自分が(i-1)回戦目まで勝ち上がる確率 * i回戦を勝ち上がる確率
+					DP[i, j] = DP[i - 1, j] * sumRatio;
+				}
+			}
+
+			for (int j = 0; j < playerNum; j++) {
+				WriteLine(DP[K, j]);
+				Dump(DP[K, j]);
+			}
+		}
+
+		double CalcWinRatioOfA(int rA, int rB) {
+			return 1 / (1 + Math.Pow(10, (rB - rA) / (double)400));
+		}
+
+#if !MYHOME
+		public static void Main(string[] args) {
+			new SolverC().Run();
+		}
+#endif
+	}
+
+	// D: サイコロ
+	public class SolverD : SolverBase
+	{
+		private const double ZERO = default(double);
+
+		public void Run() {
+			#pragma warning disable RECS0018 // 等値演算子による浮動小数点値の比較
+			var ary = ReadLongArray();
+			int N = (int)ary[0];
+			var D = ary[1];
+			if (D == 1) {
+				// ※ほんとは、1は特別扱いしなくても大丈夫だけど
+				WriteLine(1.0);
+				return;
+			}
+
+			// Dを素因数分解
+			int p2 = 0, p3 = 0, p5 = 0;
+			var d = D;
+			while (d % 2 == 0) {
+				++p2;
+				d /= 2;
+			}
+			while (d % 3 == 0) {
+				++p3;
+				d /= 3;
+			}
+			while (d % 5 == 0) {
+				++p5;
+				d /= 5;
+			}
+
+			// 素因数分解の結果2,3,5以外の因数が残った場合は絶対にその倍数にはならない
+			if (1 < d) {
+				WriteLine(0.0);
+				return;
+			}
+
+			// 多次元DP
+			// DP[ターン, 素因数2の個数, 同3, 同5] = この状況になる確率
+			//   ※メモリー節約のため、ターンは2セット分の領域を使い回す
+			double[,,,] DP = new double[2, p2 + 1, p3 + 1, p5 + 1];
+			// 初期値
+			DP[0, 0, 0, 0] = 1.0;
+
+			Action<int> DumpTurn = (int n) => {
+#if MYHOME
+				Dump($"Turn {n} ==================");
+				for (int x = 0; x < p2 + 1; x++) {
+					for (int y = 0; y < p3 + 1; y++) {
+						for (int z = 0; z < p5 + 1; z++) {
+							Dump($"[{x},{y},{z}] = {DP[n % 2 ^ 1, x, y, z]}");
+						}
+					}
+				}
+#endif
+			};
+
+			// N回サイコロを振る
+			for (int i = 0; i < N; i++) {
+				// 使用するメモリー領域の切り替え
+				int cur = (i % 2);
+				int tar = 1 ^ cur;  // curが1だったら0、0だったら1。※ ^ は排他的論理和
+
+				// target側の0クリ
+				for (int x = 0; x < p2 + 1; x++) {
+					for (int y = 0; y < p3 + 1; y++) {
+						for (int z = 0; z < p5 + 1; z++) {
+							DP[tar, x, y, z] = ZERO;
+						}
+					}
+				}
+
+				// 2,3,5の3重ループ
+				for (int x = 0; x < p2+1; x++) {
+					for (int y = 0; y < p3+1; y++) {
+						for (int z = 0; z < p5+1; z++) {
+							if (DP[cur, x, y, z] == ZERO) continue;
+
+							// 配るDP
+							// 配布元の確率を6等分して、1~6 の配布先に分配していく
+							// 1が出るなら
+							DP[tar, x, y, z] += DP[cur, x, y, z] / 6.0;
+							// 2 ※所定数以上はすべて配列末尾に加算していく
+							DP[tar, Math.Min(x+1, p2), y, z] += DP[cur, x, y, z] / 6.0;
+							// 3
+							DP[tar, x, Math.Min(y+1, p3), z] += DP[cur, x, y, z] / 6.0;
+							// 4
+							DP[tar, Math.Min(x + 2, p2), y, z] += DP[cur, x, y, z] / 6.0;
+							// 5
+							DP[tar, x, y, Math.Min(z+1, p5)] += DP[cur, x, y, z] / 6.0;
+							// 6
+							DP[tar, Math.Min(x + 1, p2), Math.Min(y + 1, p3), z] += DP[cur, x, y, z] / 6.0;
+						}
+					}
+				}
+				DumpTurn(i);
+
+#pragma warning restore RECS0018 // 等値演算子による浮動小数点値の比較
+			}
+
+			double ans = DP[N % 2, p2, p3, p5];
+			WriteLine(ans);
+		}
+
+#if !MYHOME
+		public static void Main(string[] args) {
+			new SolverD().Run();
 		}
 #endif
 	}
