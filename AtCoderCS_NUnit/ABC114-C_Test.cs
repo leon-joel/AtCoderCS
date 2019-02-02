22cd37645d3229c8ad183e7d2550011a68eccced
Leon <leopard10000@gmail.com>
2019-02-02 23:38:32 +0900

    ABC114-C: 755（七五三数）: Youtube解説通りDFSで実装 ※桁DPではない単純な再帰
    PDF解説を読んでRubyで実装していたが、それよりも更にシンプルに実装。
    https://atcoder.jp/contests/abc114/tasks/abc114_c

---
+++ AtCoderCS_NUnit/ABC114-C_Test.cs
@@ -0,0 +1,104 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ABC114.C
+{
+	public static class TestDataFactory
+	{
+		public static TestData[] Cases() {
+			return new TestData[] {
+				new TestData("Test1",
+@"575
+",
+@"4"),
+				new TestData("Test2",
+@"3600
+",
+@"13"),
+				new TestData("Test3",
+@"999999999
+",
+@"26484"),
+				new TestData("Test4",
+@"1
+",
+@"0"),
+				new TestData("Test5",
+@"99
+",
+@"0"),
+				new TestData("Test6",
+@"357
+",
+@"1"),
+//				new TestData("Test7",
+//@"2 7
+//",
+//@"2"),
+//				new TestData("Test8",
+//@"3 1
+//3 2 1
+//3
+//",
+//@"1"),
+			};
+		}
+	}
+
+	public class NunitSolver : Solver
+	{
+		readonly StringReader _reader;
+		readonly StringBuilder _sb;
+		public NunitSolver(string input, StringBuilder writer) {
+			_reader = new StringReader(input);
+			_sb = writer;
+		}
+		override protected string ReadLine() => _reader.ReadLine();
+		override protected int ReadInt() => int.Parse(_reader.ReadLine());
+		override protected long ReadLong() => long.Parse(_reader.ReadLine());
+		override protected string[] ReadStringArray() => _reader.ReadLine().Split(' ');
+		override protected int[] ReadIntArray() => _reader.ReadLine().Split(' ').Select<string, int>(s => int.Parse(s)).ToArray();
+		override protected long[] ReadLongArray() => _reader.ReadLine().Split(' ').Select<string, long>(s => long.Parse(s)).ToArray();
+		override protected double[] ReadDoubleArray() => _reader.ReadLine().Split(' ').Select<string, double>(s => double.Parse(s)).ToArray();
+		override protected void WriteLine(string line) => _sb.AppendLine(line);
+		override protected void WriteLine(double d) => _sb.AppendLine($"{d:F9}");
+		override protected void WriteLine<T>(T value) => _sb.AppendLine(value.ToString());
+	}
+	[TestFixture()]
+	public class SolverTest
+	{
+		[TestCaseSource(typeof(TestDataFactory), "Cases")]
+		public void TestCase(TestData data) {
+			var sb = new StringBuilder();
+			var solver = new NunitSolver(data.Input, sb);
+			var sw = Stopwatch.StartNew();
+			solver.Run();
+			sw.Stop();
+			Console.WriteLine($"Elapsed: {sw.Elapsed}");
+			// 文字列比較
+			Assert.AreEqual(data.Expected, sb.ToString().TrimEnd());
+			// 浮動小数比較
+			//Assert.AreEqual(double.Parse(data.Expected), double.Parse(sb.ToString().TrimEnd()), 0.000000001);
+		}
+	}
+
+	public class TestData
+	{
+		public string Name { get; private set; }
+		public string Input { get; private set; }
+		public string Expected { get; private set; }
+
+		public override string ToString() => Name;
+
+		public TestData(string name, string input, string expected) {
+			Name = name;
+			Input = input;
+			Expected = expected.Replace("\r\n", "\n");
+		}
+	}
+}
