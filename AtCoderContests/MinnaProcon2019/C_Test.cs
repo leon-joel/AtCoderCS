0559681059c2cb7ccd509039ea846584ba267826
Leon <leopard10000@gmail.com>
2019-02-10 12:06:30 +0900

    MinnaProCon2019: ABCの3完。Dは未実装。
    https://atcoder.jp/contests/yahoo-procon2019-qual

---
+++ AtCoderContests/MinnaProcon2019/C_Test.cs
@@ -0,0 +1,138 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MinnaProCon.C
+{
+	public static class TestDataFactory
+	{
+		public static TestData[] Cases() {
+			return new TestData[] {
+				new TestData("Test1",
+@"4 2 6
+",
+@"7"),
+				new TestData("Test1r",
+@"7 3 4
+",
+@"8"),
+				new TestData("Test2",
+@"314159265 35897932 384626433
+",
+@"48518828981938099"),
+				new TestData("Test3",
+@"3 3 100
+",
+@"4"),
+//				new TestData("Test4",
+//@"5
+//2 1 1 1 2
+//",
+//@"3"),
+//				new TestData("Test5",
+//@"100
+//100 99 98 97 96 95 94 93 92 91 90 89 88 87 86 85 84 83 82 81 80 79 78 77 76 75 74 73 72 71 70 69 68 67 66 65 64 63 62 61 60 59 58 57 56 55 54 53 52 51 50 49 48 47 46 45 44 43 42 41 40 39 38 37 36 35 34 33 32 31 30 29 28 27 26 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10 9 8 7 6 5 4 3 2 1
+//",
+//@"100"),
+//				new TestData("Test6",
+//@"1 2
+//2
+//2 1
+//",
+//@"1"),
+//				new TestData("Test7",
+//@"3 1
+//3 2 1
+//3
+//",
+//@"1"),
+//				new TestData("Test8",
+//@"3 3
+//1 2 3
+//1 2 3",
+//@"0"),
+//				new TestData("Test8-1",
+//@"1000000005
+//1",
+//@"1000000005"),
+//				new TestData("Test9",
+//@"1000000008
+//1",
+//@"1"),
+//				new TestData("Test9-1",
+//@"1000000009
+//1",
+//@"2"),
+
+//				new TestData("Test10",
+//@"2000000013
+//1",
+//@"1000000006"),
+//				new TestData("Test11",
+//@"2000000014
+//1",
+//@"0"),
+//				new TestData("Test12",
+//@"2000000015
+//1",
+//@"1"),
+			};
+		}
+	}
+
+	public class NunitSolver : Solver
+	{
+		readonly StringReader _reader;
+		readonly StringBuilder _sb;
+		public NunitSolver(string input, StringBuilder writer) {
+			_reader = new StringReader(input);
+			_sb = writer;
+		}
+		override protected string ReadLine() => _reader.ReadLine();
+		override protected int ReadInt() => int.Parse(_reader.ReadLine());
+		override protected long ReadLong() => long.Parse(_reader.ReadLine());
+		override protected string[] ReadStringArray() => _reader.ReadLine().Split(' ');
+		override protected int[] ReadIntArray() => _reader.ReadLine().Split(' ').Select<string, int>(s => int.Parse(s)).ToArray();
+		override protected long[] ReadLongArray() => _reader.ReadLine().Split(' ').Select<string, long>(s => long.Parse(s)).ToArray();
+		override protected double[] ReadDoubleArray() => _reader.ReadLine().Split(' ').Select<string, double>(s => double.Parse(s)).ToArray();
+		override protected void WriteLine(string line) => _sb.AppendLine(line);
+		override protected void WriteLine(double d) => _sb.AppendLine($"{d:F9}");
+		override protected void WriteLine<T>(T value) => _sb.AppendLine(value.ToString());
+	}
+	[TestFixture()]
+	public class SolverTest
+	{
+		[TestCaseSource(typeof(TestDataFactory), "Cases")]
+		public void TestCase(TestData data) {
+			var sb = new StringBuilder();
+			var solver = new NunitSolver(data.Input, sb);
+			var sw = Stopwatch.StartNew();
+			solver.Run();
+			sw.Stop();
+			Console.WriteLine($"Elapsed: {sw.Elapsed}");
+			// 文字列比較
+			Assert.AreEqual(data.Expected, sb.ToString().TrimEnd());
+			// 浮動小数比較
+			//Assert.AreEqual(double.Parse(data.Expected), double.Parse(sb.ToString().TrimEnd()), 0.000000001);
+		}
+	}
+
+	public class TestData
+	{
+		public string Name { get; private set; }
+		public string Input { get; private set; }
+		public string Expected { get; private set; }
+
+		public override string ToString() => Name;
+
+		public TestData(string name, string input, string expected) {
+			Name = name;
+			Input = input;
+			Expected = expected.Replace("\r\n", "\n");
+		}
+	}
+}
---
+++ AtCoderContests/MinnaProcon2019/D_Test.cs
@@ -0,0 +1,142 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace MinnaProCon.D
+{
+	public static class TestDataFactory
+	{
+		public static TestData[] Cases() {
+			return new TestData[] {
+				new TestData("Test1",
+@"4
+1
+0
+2
+3
+",
+@"1"),
+				new TestData("Test2",
+@"8
+2
+0
+0
+2
+1
+3
+4
+1
+",
+@"3"),
+				new TestData("Test3",
+@"7
+314159265
+358979323
+846264338
+327950288
+419716939
+937510582
+0
+",
+@"1"),
+				new TestData("Test4",
+@"3 10
+8 1 2
+",
+@"26"),
+				new TestData("Test5",
+@"3 4
+4 0 0
+",
+@"13"),
+				new TestData("Test6",
+@"1 1
+1000000000000
+",
+@"1000000000001"),
+				new TestData("Test7",
+@"3 10
+8 7 6
+",
+@"30"),
+				new TestData("Test8",
+@"3 4
+1 2 4
+",
+@"11"),
+				new TestData("Test9",
+@"3 4
+1 1 4",
+@"12"),
+				new TestData("Test10",
+@"1 4
+1",
+@"5"),
+				new TestData("Test11",
+@"1 7
+1",
+@"7"),
+				new TestData("Test12",
+@"5 2
+0 0 0 2 2",
+@"9"),
+			};
+		}
+	}
+
+	public class NunitSolver : Solver
+	{
+		readonly StringReader _reader;
+		readonly StringBuilder _sb;
+		public NunitSolver(string input, StringBuilder writer) {
+			_reader = new StringReader(input);
+			_sb = writer;
+		}
+		override protected string ReadLine() => _reader.ReadLine();
+		override protected int ReadInt() => int.Parse(_reader.ReadLine());
+		override protected long ReadLong() => long.Parse(_reader.ReadLine());
+		override protected string[] ReadStringArray() => _reader.ReadLine().Split(' ');
+		override protected int[] ReadIntArray() => _reader.ReadLine().Split(' ').Select<string, int>(s => int.Parse(s)).ToArray();
+		override protected long[] ReadLongArray() => _reader.ReadLine().Split(' ').Select<string, long>(s => long.Parse(s)).ToArray();
+		override protected double[] ReadDoubleArray() => _reader.ReadLine().Split(' ').Select<string, double>(s => double.Parse(s)).ToArray();
+		override protected void WriteLine(string line) => _sb.AppendLine(line);
+		override protected void WriteLine(double d) => _sb.AppendLine($"{d:F9}");
+		override protected void WriteLine<T>(T value) => _sb.AppendLine(value.ToString());
+	}
+	[TestFixture()]
+	public class SolverTest
+	{
+		[TestCaseSource(typeof(TestDataFactory), "Cases")]
+		public void TestCase(TestData data) {
+			var sb = new StringBuilder();
+			var solver = new NunitSolver(data.Input, sb);
+			var sw = Stopwatch.StartNew();
+			solver.Run();
+			sw.Stop();
+			Console.WriteLine($"Elapsed: {sw.Elapsed}");
+			// 文字列比較
+			Assert.AreEqual(data.Expected, sb.ToString().TrimEnd());
+			// 浮動小数比較
+			//Assert.AreEqual(double.Parse(data.Expected), double.Parse(sb.ToString().TrimEnd()), 0.000000001);
+		}
+	}
+
+	public class TestData
+	{
+		public string Name { get; private set; }
+		public string Input { get; private set; }
+		public string Expected { get; private set; }
+
+		public override string ToString() => Name;
+
+		public TestData(string name, string input, string expected) {
+			Name = name;
+			Input = input;
+			Expected = expected.Replace("\r\n", "\n");
+		}
+	}
+}
