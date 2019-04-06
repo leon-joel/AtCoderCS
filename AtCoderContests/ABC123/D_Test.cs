d5c4ddccff6c14548620dc3543853b14d28f4372
Leon <leopard10000@gmail.com>
2019-04-06 22:34:25 +0900

    ABC123: ノーミス全完。過去最高213位！

---
+++ AtCoderContests/ABC123/D_Test.cs
@@ -0,0 +1,178 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ABC123.D
+{
+	public static class TestDataFactory
+	{
+		public static TestData[] Cases() {
+			return new TestData[] {
+				new TestData("Test1",
+@"2 2 2 8
+4 6
+1 5
+3 8
+",
+@"19
+17
+15
+14
+13
+12
+10
+8
+"),
+				new TestData("Test2",
+@"3 3 3 5
+1 10 100
+2 20 200
+1 10 100
+",
+@"400
+310
+310
+301
+301
+"),
+				new TestData("Test3",
+@"10 10 10 20
+7467038376 5724769290 292794712 2843504496 3381970101 8402252870 249131806 6310293640 6690322794 6082257488
+1873977926 2576529623 1144842195 1379118507 6003234687 4925540914 3902539811 3326692703 484657758 2877436338
+4975681328 8974383988 2882263257 7690203955 514305523 6679823484 4263279310 585966808 3752282379 620585736
+",
+@"23379871545
+22444657051
+22302177772
+22095691512
+21667941469
+21366963278
+21287912315
+21279176669
+21160477018
+21085311041
+21059876163
+21017997739
+20703329561
+20702387965
+20590247696
+20383761436
+20343962175
+20254073196
+20210218542
+20150096547
+"),
+				new TestData("Test4",
+@"1 1 1 1
+1
+2
+3
+",
+@"6"),
+				new TestData("Test5",
+@"5 3
+5 3 2
+",
+@"5"),
+//				new TestData("Test6",
+//@"1 2
+//2
+//2 1
+//",
+//@"1"),
+//				new TestData("Test7",
+//@"3 1
+//3 2 1
+//3
+//",
+//@"1"),
+//				new TestData("Test8",
+//@"3 3
+//1 2 3
+//1 2 3",
+//@"0"),
+//				new TestData("Test8-1",
+//@"1000000005
+//1",
+//@"1000000005"),
+//				new TestData("Test9",
+//@"1000000008
+//1",
+//@"1"),
+//				new TestData("Test9-1",
+//@"1000000009
+//1",
+//@"2"),
+
+//				new TestData("Test10",
+//@"2000000013
+//1",
+//@"1000000006"),
+//				new TestData("Test11",
+//@"2000000014
+//1",
+//@"0"),
+//				new TestData("Test12",
+//@"2000000015
+//1",
+//@"1"),
+			};
+		}
+	}
+
+	public class NunitSolver : Solver
+	{
+		readonly StringReader _reader;
+		readonly StringBuilder _sb;
+		public NunitSolver(string input, StringBuilder writer) {
+			_reader = new StringReader(input);
+			_sb = writer;
+		}
+		override protected string ReadLine() => _reader.ReadLine();
+		override protected int ReadInt() => int.Parse(_reader.ReadLine());
+		override protected long ReadLong() => long.Parse(_reader.ReadLine());
+		override protected string[] ReadStringArray() => _reader.ReadLine().Split(' ');
+		override protected int[] ReadIntArray() => _reader.ReadLine().Split(' ').Select<string, int>(s => int.Parse(s)).ToArray();
+		override protected long[] ReadLongArray() => _reader.ReadLine().Split(' ').Select<string, long>(s => long.Parse(s)).ToArray();
+		override protected double[] ReadDoubleArray() => _reader.ReadLine().Split(' ').Select<string, double>(s => double.Parse(s)).ToArray();
+		override protected void WriteLine(string line) => _sb.AppendLine(line);
+		override protected void WriteLine(double d) => _sb.AppendLine($"{d:F9}");
+		override protected void WriteLine<T>(T value) => _sb.AppendLine(value.ToString());
+	}
+	[TestFixture()]
+	public class SolverTest
+	{
+		[TestCaseSource(typeof(TestDataFactory), "Cases")]
+		public void TestCase(TestData data) {
+			var sb = new StringBuilder();
+			var solver = new NunitSolver(data.Input, sb);
+			var sw = Stopwatch.StartNew();
+			solver.Run();
+			sw.Stop();
+			Console.WriteLine($"Elapsed: {sw.Elapsed}");
+			// 文字列比較
+			Assert.AreEqual(data.Expected.TrimEnd(), sb.ToString().TrimEnd());
+			// 浮動小数比較
+			//Assert.AreEqual(double.Parse(data.Expected), double.Parse(sb.ToString().TrimEnd()), 0.000000001);
+		}
+	}
+
+	public class TestData
+	{
+		public string Name { get; private set; }
+		public string Input { get; private set; }
+		public string Expected { get; private set; }
+
+		public override string ToString() => Name;
+
+		public TestData(string name, string input, string expected) {
+			Name = name;
+			Input = input;
+			Expected = expected.Replace("\r\n", "\n");
+		}
+	}
+}
