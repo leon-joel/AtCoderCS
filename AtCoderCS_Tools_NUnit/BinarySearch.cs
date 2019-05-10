b86a6d8c71f43baf1d55122ea1c013bdff0ab206
Leon <leopard10000@gmail.com>
2019-05-10 23:48:39 +0900

    ABC091-D: 二分探索で実装。大きいサンプルは全部TLEしてしまう。
    二分探索ではなく、A側もソートして、B側をシーケンシャルに1度だけ探索するようにしたほうがいいかも。

--- AtCoderCS_Tools_NUnit/BinarySearch.cs
+++ AtCoderCS_Tools_NUnit/BinarySearch.cs
@@ -1,7 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Tools
 {
 	public static partial class Util {
+		public static bool IsOK<T>(IList<T> ary, int idx, T key) where T : IComparable, IComparable<T> {
+			// key <= ary[idx] と同じ意味
+			return key.CompareTo(ary[idx]) <= 0;
+		}
 		/// <summary>
 		/// 二分探索
 		/// ※条件を満たす最小のidxを返す
@@ -11,10 +17,18 @@ namespace Tools
 		/// </summary>
 		/// <param name="ary">探索対象配列 ★ソート済みであること</param>
 		/// <param name="key">探索値 ※これ以上の値を持つ（IsOKがtrueを返す）最小のindexを返す</param>
-		public static int BinarySearch<T>(T[] ary, T key) where T : IComparable, IComparable<T> {
-			int left = -1;
-			int right = ary.Length;
-
+		public static int BinarySearch<T>(IList<T> ary, T key) where T : IComparable, IComparable<T> {
+			return BinarySearch(ary, -1, ary.Count, key);
+		}
+		/// <summary>
+		/// 範囲付き二分探索
+		/// ※条件を満たす最小のidxを返す
+		/// 例) 0 1 2 3 4 5 6 7 8 9
+		/// [3, 4] からの探索の場合、l=2, r=5 を与える
+		/// すべてが条件を満たす場合は l+1 が返される
+		/// 条件を満たすものがない場合は r が返される
+		/// </summary>
+		public static int BinarySearch<T>(IList<T> ary, int left, int right, T key) where T : IComparable, IComparable<T> {
 			while (1 < right - left) {
 				var mid = left + (right - left) / 2;
 
@@ -27,10 +41,6 @@ namespace Tools
 
 			// left は条件を満たさない最大の値、right は条件を満たす最小の値になっている
 			return right;
-		}
-		public static bool IsOK<T>(T[] ary, int idx, T key) where T : IComparable, IComparable<T> {
-			// key <= ary[idx] と同じ意味
-			return key.CompareTo(ary[idx]) <= 0;
 		}
 	}
 }
--- AtCoderCS_Tools_NUnit/Tests/BinarySearch_Test.cs
+++ AtCoderCS_Tools_NUnit/Tests/BinarySearch_Test.cs
@@ -1,5 +1,8 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+
 namespace Tools
 {
 	[TestFixture()]
@@ -21,6 +24,19 @@ namespace Tools
 			// 該当なしの場合（全部falseの場合）は ary.Length を返す
 			Assert.AreEqual(10, Util.BinarySearch(ary, 10));
 			Assert.AreEqual(10, Util.BinarySearch(ary, 100));
+
+			// 範囲付きBinarySearch
+			// 0 1 2 3 4 5 6 7 8 9
+			// [3, 4] からの探索の場合、l=2, r=5 を与える
+			// すべてが条件を満たす場合は l+1 が返される
+			Assert.AreEqual(3, Util.BinarySearch(ary, 2, 5, 1));
+			Assert.AreEqual(3, Util.BinarySearch(ary, 2, 5, 2));
+			Assert.AreEqual(3, Util.BinarySearch(ary, 2, 5, 3));
+
+			Assert.AreEqual(4, Util.BinarySearch(ary, 2, 5, 4));
+			// 条件を満たすものがない場合は r が返される
+			Assert.AreEqual(5, Util.BinarySearch(ary, 2, 5, 5));
+			Assert.AreEqual(5, Util.BinarySearch(ary, 2, 5, 6));
 		}
 	}
 }
