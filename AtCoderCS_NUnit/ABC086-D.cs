6bfa52bc434fce2e22df98de45b06091202b2f9f
Leon <leopard10000@gmail.com>
2019-05-24 23:32:40 +0900

    ABC086-D: 2次元累積和 問題 ※初MLE （メモリーオーバー）発生w

---
+++ AtCoderCS_NUnit/ABC086-D.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace ABC086.D
+{
+	using static Util;
+
+	public class Solver : SolverBase
+	{
+		int K = 0;
+		int k2 = 0;
+		public void Run() {
+			var ary = ReadIntArray();
+			var N = ary[0];
+			K = ary[1];
+
+			k2 = K * 2;
+			// 2次元累積和計算用のmod grid
+			int wSum = 0, bSum = 0;
+			var bg = new int[k2, k2];   // 黒
+			var wg = new int[k2, k2];	// 白
+			for (int i = 0; i < N; i++) {
+				var sary = ReadStringArray();
+				var x = int.Parse(sary[0]) % k2;
+				var y = int.Parse(sary[1]) % k2;
+				var s = sary[2];
+
+				if (s == "B") {
+					bg[x, y] += 1;
+					++bSum;
+				} else {
+					wg[x, y] += 1;
+					++wSum;
+				}
+			}
+
+			// 2次元累積和
+			var bs = new int[k2 + 1, k2 + 1];
+			var ws = new int[k2 + 1, k2 + 1];
+			for (int x = 0; x < k2; x++) {
+				for (int y = 0; y < k2; y++) {
+					bs[x + 1, y + 1] = bs[x, y + 1] + bs[x + 1, y] - bs[x, y] + bg[x, y];
+					ws[x + 1, y + 1] = ws[x, y + 1] + ws[x + 1, y] - ws[x, y] + wg[x, y];
+				}
+			}
+
+			// 黒/白マス左上座標の範囲で総当たり
+			int ans = 0;
+			for (int x = 0; x < K; x++) {
+				for (int y = 0; y < K; y++) {
+					var bCnt = SumRegion(bs, x, y);
+					var wCnt = wSum - SumRegion(ws, x, y);
+
+					ReplaceIfBigger(ref ans, bCnt + wCnt);
+				
+					bCnt = bSum - bCnt;
+					wCnt = wSum - wCnt;
+					ReplaceIfBigger(ref ans, bCnt + wCnt);
+				}
+			}
+
+			WriteLine(ans);
+		}
+		// センター領域の左上座標を与える
+		int SumRegion(int[,] g, int x, int y) {
+			// センター領域
+			var sum = g[x + K, y + K] - g[x + K, y] - g[x, y + K] + g[x, y];
+			// 左上領域
+			sum += g[x, y];
+			// 左下領域
+			sum += g[k2, y] - g[x + K, y];
+			// 右上領域
+			sum += g[x, k2] - g[x, y + K];
+			// 右下領域
+			sum += g[k2, k2] - g[k2, y + K] - g[x + K, k2] + g[x + K, y + K];
+			return sum;
+		}
+
+#if !MYHOME
+		static void Main(string[] args) {
+			new Solver().Run();
+		}
+#endif
+	}
+
+	public static class Util
+	{
+		public readonly static long MOD = 1000000007;
+
+		public static string DumpToString<T>(IEnumerable<T> array) where T : IFormattable {
+			var sb = new StringBuilder();
+			foreach (var item in array) {
+				sb.Append(item);
+				sb.Append(", ");
+			}
+			return sb.ToString();
+		}
+
+		public static void InitArray<T>(T[] ary, T value) {
+			for (int i = 0; i < ary.Length; i++) {
+				ary[i] = value;
+			}
+		}
+		public static void InitDP2<T>(T[,] dp, T value) {
+			for (int i = 0; i < dp.GetLength(0); i++) {
+				for (int j = 0; j < dp.GetLength(1); j++) {
+					dp[i, j] = value;
+				}
+			}
+		}
+		public static void InitDP3<T>(T[,,] dp, T value) {
+			for (int i = 0; i < dp.GetLength(0); i++) {
+				for (int j = 0; j < dp.GetLength(1); j++) {
+					for (int k = 0; k < dp.GetLength(2); k++) {
+						dp[i, j, k] = value;
+					}
+				}
+			}
+		}
+
+		public static T Max<T>(params T[] nums) where T : IComparable {
+			if (nums.Length == 0) return default(T);
+
+			T max = nums[0];
+			for (int i = 1; i < nums.Length; i++) {
+				max = max.CompareTo(nums[i]) > 0 ? max : nums[i];
+			}
+			return max;
+		}
+		public static T Min<T>(params T[] nums) where T : IComparable {
+			if (nums.Length == 0) return default(T);
+
+			T min = nums[0];
+			for (int i = 1; i < nums.Length; i++) {
+				min = min.CompareTo(nums[i]) < 0 ? min : nums[i];
+			}
+			return min;
+		}
+
+		/// <summary>
+		/// ソート済み配列 ary に同じ値の要素が含まれている？
+		/// ※ソート順は昇順/降順どちらでもよい
+		/// </summary>
+		public static bool HasDuplicateInSortedArray<T>(T[] ary) where T : IComparable, IComparable<T> {
+			if (ary.Length <= 1) return false;
+
+			var lastNum = ary[ary.Length - 1];
+
+			foreach (var n in ary) {
+				if (lastNum.CompareTo(n) == 0) {
+					return true;
+				} else {
+					lastNum = n;
+				}
+			}
+			return false;
+		}
+
+		public static void ReplaceIfBigger<T>(ref T r, T v) where T : IComparable {
+			if (r.CompareTo(v) < 0) r = v;
+		}
+		public static void ReplaceIfSmaller<T>(ref T r, T v) where T : IComparable {
+			if (0 < r.CompareTo(v)) r = v;
+		}
+
+		/// <summary>
+		/// 二分探索
+		/// ※条件を満たす最小のidxを返す
+		/// ※満たすものがない場合は ary.Length を返す
+		/// ※『aryの先頭側が条件を満たさない、末尾側が条件を満たす』という前提
+		/// ただし、IsOK(...)の戻り値を逆転させれば、逆でも同じことが可能。
+		/// </summary>
+		/// <param name="ary">探索対象配列 ★ソート済みであること</param>
+		/// <param name="key">探索値 ※これ以上の値を持つ（IsOKがtrueを返す）最小のindexを返す</param>
+		public static int BinarySearch<T>(T[] ary, T key) where T : IComparable, IComparable<T> {
+			int left = -1;
+			int right = ary.Length;
+
+			while (1 < right - left) {
+				var mid = left + (right - left) / 2;
+
+				if (IsOK(ary, mid, key)) {
+					right = mid;
+				} else {
+					left = mid;
+				}
+			}
+
+			// left は条件を満たさない最大の値、right は条件を満たす最小の値になっている
+			return right;
+		}
+		public static bool IsOK<T>(T[] ary, int idx, T key) where T : IComparable, IComparable<T> {
+			// key <= ary[idx] と同じ意味
+			return key.CompareTo(ary[idx]) <= 0;
+		}
+	}
+
+	public class SolverBase
+	{
+		virtual protected string ReadLine() => Console.ReadLine();
+		virtual protected int ReadInt() => int.Parse(ReadLine());
+		//virtual protected void ReadInt2(out int x, out int y) {
+		//	var aryS = ReadLine().Split(' ');
+		//	x = int.Parse(aryS[0]);
+		//	y = int.Parse(aryS[1]);
+		//}
+		virtual protected long ReadLong() => long.Parse(ReadLine());
+		virtual protected string[] ReadStringArray() => ReadLine().Split(' ');
+		virtual protected int[] ReadIntArray() => ReadLine().Split(' ').Select<string, int>(s => int.Parse(s)).ToArray();
+		virtual protected long[] ReadLongArray() => ReadLine().Split(' ').Select<string, long>(s => long.Parse(s)).ToArray();
+		virtual protected double[] ReadDoubleArray() => ReadLine().Split(' ').Select<string, double>(s => double.Parse(s)).ToArray();
+		virtual protected void WriteLine(string line) => Console.WriteLine(line);
+		virtual protected void WriteLine(double d) => Console.WriteLine($"{d:F9}");
+		virtual protected void WriteLine<T>(T value) where T : IFormattable => Console.WriteLine(value);
+
+		[Conditional("DEBUG")]
+		protected void Dump(string s) => Console.WriteLine(s);
+		[Conditional("DEBUG")]
+		protected void Dump(char c) => Console.WriteLine(c);
+		[Conditional("DEBUG")]
+		protected void Dump(int x) => Console.WriteLine(x);
+		[Conditional("DEBUG")]
+		protected void Dump(double d) => Console.WriteLine($"{d:F9}");
+		[Conditional("DEBUG")]
+		protected void Dump<T>(IEnumerable<T> array) where T : IFormattable {
+			string s = Util.DumpToString(array);
+			// Consoleに出力すると、UnitTestの邪魔をしないというメリットあり。
+			Console.WriteLine(s);
+			//_writer.WriteLine(s);
+		}
+		[Conditional("DEBUG")]
+		protected void DumpGrid<T>(IEnumerable<IEnumerable<T>> arrayOfArray) where T : IFormattable {
+			var sb = new StringBuilder();
+			foreach (var a in arrayOfArray) {
+				sb.AppendLine(Util.DumpToString(a));
+			}
+			// Consoleに出力すると、UnitTestの邪魔をしないというメリットあり。
+			Console.WriteLine(sb.ToString());
+			//_writer.WriteLine(sb.ToString());
+		}
+		[Conditional("DEBUG")]
+		protected void DumpDP<T>(T[,] dp) where T : IFormattable {
+			var sb = new StringBuilder();
+			for (int i = 0; i < dp.GetLength(0); i++) {
+				for (int j = 0; j < dp.GetLength(1); j++) {
+					sb.Append(dp[i, j]);
+					sb.Append(", ");
+				}
+				sb.AppendLine();
+			}
+			Console.WriteLine(sb.ToString());
+		}
+	}
+}
