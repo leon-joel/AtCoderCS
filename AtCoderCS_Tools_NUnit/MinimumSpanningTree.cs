324813319e1352e0244bc730d7241ff95224c685
Leon <leopard10000@gmail.com>
2019-05-29 21:49:45 +0900

    最小全域木コスト計算アルゴリズム『クラスカル法』を実装

---
+++ AtCoderCS_Tools_NUnit/MinimumSpanningTree.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tools
+{
+	public class Edge : IComparable<Edge>
+	{
+		public int V0;
+		public int V1;
+		public int Cost;
+
+		public Edge(int v0, int v1, int cost) {
+			V0 = v0;
+			V1 = v1;
+			Cost = cost;
+		}
+
+		public int CompareTo(Edge other) {
+			return Cost.CompareTo(other.Cost);
+		}
+	}
+	/// <summary>
+	/// 無向グラフにおける最小全域木アルゴリズム
+	/// ★ UnionFindTree が必要
+	/// </summary>
+	public class MST
+	{
+		// 頂点数
+		readonly public int NumOfV;
+
+		// すべての辺
+		readonly public List<Edge> Edges;
+
+		/// <summary>
+		/// コンストラクタ: 頂点数を与える
+		/// </summary>
+		public MST(int numOfV) {
+			NumOfV = numOfV;
+			Edges = new List<Edge>();
+		}
+		/// <summary>
+		/// コンストラクタ: 頂点数とエッジ数を与える ※エッジ数は ListへのAddを効率よく行うため
+		/// </summary>
+		public MST(int numOfV, int numOfE) {
+			NumOfV = numOfV;
+			Edges = new List<Edge>(numOfE);
+		}
+		/// <summary>
+		/// コンストラクタ: エッジ数をエッジリストを与える
+		/// ※edgesはCalcの最初にCostでソートするので事前ソートは不要
+		/// </summary>
+		public MST(int numOfV, List<Edge> edges) {
+			NumOfV = numOfV;
+			Edges = edges;
+		}
+
+		public void Add(int v0, int v1, int cost) {
+			Edges.Add(new Edge(v0, v1, cost));
+		}
+
+		/// <summary>
+		/// クラスカル法
+		/// </summary>
+		/// <returns>最小全域木のコスト合計</returns>
+		public int CalcMinCostByKruskal() {
+			Edges.Sort();
+
+			var uft = new UnionFindTree(NumOfV);
+
+			int minCost = 0;
+
+			foreach (var e in Edges) {
+				if (!uft.IsSame(e.V0, e.V1)) {
+					// 別グループ、つまりこの辺を採用しても閉路ができない。よって採用！
+					minCost += e.Cost;
+					uft.Unite(e.V0, e.V1);
+				}
+			}
+
+			return minCost;
+		}
+	}
+}
---
+++ AtCoderCS_Tools_NUnit/Tests/MST_Test.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Tools
+{
+	using static Util;
+
+	[TestFixture()]
+	public class MSTTests
+	{
+		[Test]
+		public void CalcMinCostByKruskalTest() {
+			var mst = new MST(5);
+			mst.Add(2, 4, 10000);
+			mst.Add(0, 1, 10);
+			mst.Add(0, 3, 5);
+			mst.Add(1, 2, 1);
+			mst.Add(1, 3, 1000);
+			mst.Add(1, 4, 500);
+			mst.Add(2, 3, 100);
+			mst.Add(3, 4, 5000);
+
+			var minCost = mst.CalcMinCostByKruskal();
+			Assert.AreEqual(516, minCost);
+		}
+	}
+}
