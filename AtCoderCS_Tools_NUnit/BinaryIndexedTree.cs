e716f64ba98b84be4dab0a8a0dbab75b876805d4
Leon <leopard10000@gmail.com>
2020-03-04 01:19:05 +0900

    ABC157: E問題: BITできれいに解ける問題

--- AtCoderCS_Tools_NUnit/BinaryIndexedTree.cs
+++ AtCoderCS_Tools_NUnit/BinaryIndexedTree.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 namespace Tools
 {
+	/// <summary>
+	/// 1-indexedなBIT
+	/// </summary>
 	public class BinaryIndexedTree
 	{
 		public int MaxNum { get; private set; }
@@ -12,15 +15,19 @@ namespace Tools
 			bit = new int[maxNum + 1];
 		}
 
-		// i の個数を x 増加
-		// ※i は1以上
+		/// <summary>
+		/// i の個数を x 増加
+		/// ※i は1以上
+		/// </summary>
 		public void Add(int i, int v) {
 			for (int x = i; x <= MaxNum; x += x & -x)
 				bit[x] += v;
 		}
 
-		// i 以下の総数を返す
-		// ※0以下のi を与えた場合は0を返す
+		/// <summary>
+		/// i 以下の総数を返す
+		/// ※0以下のi を与えた場合は0を返す
+		/// </summary>
 		public int Sum(int i) {
 			int ret = 0;
 			for (int x = i; 0 < x; x -= x & -x)
@@ -28,12 +35,16 @@ namespace Tools
 			return ret;
 		}
 
-		// [lower, upper] つまり lower以上 upper以下 の総数を返す
+		/// <summary>
+		/// [lower, upper] つまり lower以上 upper以下 の総数を返す 
+		/// </summary>
 		public int RangeSum(int lower, int upper) {
 			return Sum(upper) - Sum(lower-1);
 		}
 
-		// i の個数を返す
+		/// <summary>
+		/// i の個数を返す
+		/// </summary>
 		public int Count(int i) {
 			return Sum(i) - Sum(i - 1);
 		}
