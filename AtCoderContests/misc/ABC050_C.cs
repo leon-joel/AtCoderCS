c0ecf666972299cbac8de1785eb4970623ebbf6b
Leon <leopard10000@gmail.com>
2020-03-10 23:14:12 +0900

    ABC050-C

---
+++ AtCoderContests/misc/ABC050_C.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace ABC050.C
+{
+	using static Util;
+
+	public class Solver : SolverBase
+	{
+		public void Run() {
+			var N = ReadInt();
+			var Ary = ReadIntArray();
+			Array.Sort(Ary);
+
+			if (N % 2 == 0) {
+				for (int i = 0; i < N / 2; i++) {
+					if (Ary[i * 2] != i * 2 + 1 ||
+						Ary[(i * 2) + 1] != i * 2 + 1) {
+						WriteLine(0);
+						return;
+					}
+				}
+			} else {
+				if (Ary[0] != 0) {
+					WriteLine(0);
+					return;
+				}
+				for (int i = 0; i < N / 2; i++) {
+					if (Ary[(i * 2) + 1] != (i + 1) * 2 ||
+						Ary[(i * 2) + 2] != (i + 1) * 2) {
+						WriteLine(0);
+						return;
+					}
+				}
+			}
+
+			// 偶数なら 2^(N/2) 通り
+			// 奇数なら 2^(N/2) 通り
+			var ans = Modulo.Pow(2, N / 2);
+			WriteLine(ans);
+		}
+
+#if !MYHOME
+		static void Main(string[] args) {
+			new Solver().Run();
+		}
+#endif
+	}
+	public class Modulo
+	{
+		private readonly static int M = 1000000007;
+
+		/// <summary>(a * b) % M</summary>
+		public static int Mul(int a, int b) {
+			return (int)(Math.BigMul(a, b) % M);
+		}
+		/// <summary>(a + b) % M</summary>
+		public static int Add(int a, int b) {
+			long v = a + b;
+			if (M <= v)
+				v = v - M + 1;
+			return (int)v;
+		}
+		/// <summary>(a - b) % M</summary>
+		public static int Sub(int a, int b) {
+			int v = a - b;
+			if (v < 0) v = M + v;
+			return v;
+		}
+
+		/// <summary>(aのm乗) % M</summary>
+		/// <see cref="https://www.youtube.com/watch?v=gdQxKESnXKs のD問題"/>
+		public static int Pow(int a, int m) {
+			switch (m) {
+				case 0:
+					return 1;
+				case 1:
+					return a % M;
+				default:
+					int p1 = Pow(a, m / 2);
+					int p2 = Mul(p1, p1);
+					return ((m % 2) == 0) ? p2 : Mul(p2, a);
+			}
+		}
+
+		/// <summary>
+		/// (a / b) % M
+		/// ※フェルマーの小定理による
+		/// </summary>
+		/// <see cref="https://www.youtube.com/watch?v=gdQxKESnXKs のD問題"/>
+		public static int Div(int a, int b) {
+			return Mul(a, Pow(b, M - 2));
+		}
+
+		/// <summary>
+		/// コンストラクタ ※n! % M および nCr % M を使用する場合には必要
+		/// </summary>
+		private readonly int[] m_facs;
+		public Modulo(int n) {
+			// x が n までの、x! % M の結果を配列に保持する
+			m_facs = new int[n + 1];
+			m_facs[0] = 1;
+			for (int i = 1; i <= n; ++i) {
+				m_facs[i] = Mul(m_facs[i - 1], i);
+			}
+		}
+
+		/// <summary>n! % M</summary>
+		public int Fac(int n) {
+			return m_facs[n];
+		}
+		/// <summary>
+		/// 組み合わせ nCr % M
+		/// </summary>
+		/// <see cref="https://www.youtube.com/watch?v=gdQxKESnXKs のD問題"/>
+		public int Ncr(int n, int r) {
+			if (n < r) { return 0; }
+			if (n == r) { return 1; }
+			int res = Fac(n);
+			res = Div(res, Fac(r));
+			res = Div(res, Fac(n - r));
+			return res;
+		}
+
+		/// <summary>
+		/// 組み合わせ nCr % M を高速に計算する
+		/// ※n!のテーブルを使用しない版
+		/// </summary>
+		public static int CalcNcr(int n, int r) {
+			if (n - r < r) return CalcNcr(n, n - r);
+
+			long ansMul = 1;
+			long ansDiv = 1;
+			for (int i = 0; i < r; i++) {
+				ansMul *= n - i;
+				ansDiv *= i + 1;
+				ansMul %= M;
+				ansDiv %= M;
+			}
+
+			return Div((int)ansMul, (int)ansDiv);
+		}
+	}
+
+	public static class Util
+	{
+		public static int Gcd(int a, int b) {
+			if (a < b)
+				// 引数を入替えて自分を呼び出す
+				return Gcd(b, a);
+			while (b != 0) {
+				var remainder = a % b;
+				a = b;
+				b = remainder;
+			}
+			return a;
+		}
+		public readonly static long MOD = 1000000007;
+
+		public static string DumpToString<T>(IEnumerable<T> array) where T : IFormattable {
+			var sb = new StringBuilder();
+			foreach (var item in array) {
+				sb.Append(item);
+				sb.Append(", ");
+			}
+			return sb.ToString();
+		}
+
+		public static void InitArray<T>(T[] ary, T value) {
+			for (int i = 0; i < ary.Length; i++) {
+				ary[i] = value;
+			}
+		}
+		public static void InitDP2<T>(T[,] dp, T value) {
+			for (int i = 0; i < dp.GetLength(0); i++) {
+				for (int j = 0; j < dp.GetLength(1); j++) {
+					dp[i, j] = value;
+				}
+			}
+		}
+		public static void InitDP3<T>(T[,,] dp, T value) {
+			for (int i = 0; i < dp.GetLength(0); i++) {
+				for (int j = 0; j < dp.GetLength(1); j++) {
+					for (int k = 0; k < dp.GetLength(2); k++) {
+						dp[i, j, k] = value;
+					}
+				}
+			}
+		}
+
+		public static T Max<T>(params T[] nums) where T : IComparable {
+			if (nums.Length == 0) return default(T);
+
+			T max = nums[0];
+			for (int i = 1; i < nums.Length; i++) {
+				max = max.CompareTo(nums[i]) > 0 ? max : nums[i];
+			}
+			return max;
+		}
+		public static T Min<T>(params T[] nums) where T : IComparable {
+			if (nums.Length == 0) return default(T);
+
+			T min = nums[0];
+			for (int i = 1; i < nums.Length; i++) {
+				min = min.CompareTo(nums[i]) < 0 ? min : nums[i];
+			}
+			return min;
+		}
+
+		/// <summary>
+		/// ソート済み配列 ary に同じ値の要素が含まれている？
+		/// ※ソート順は昇順/降順どちらでもよい
+		/// </summary>
+		public static bool HasDuplicateInSortedArray<T>(T[] ary) where T : IComparable, IComparable<T> {
+			if (ary.Length <= 1) return false;
+
+			var lastNum = ary[ary.Length - 1];
+
+			foreach (var n in ary) {
+				if (lastNum.CompareTo(n) == 0) {
+					return true;
+				} else {
+					lastNum = n;
+				}
+			}
+			return false;
+		}
+
+		public static bool ReplaceIfBigger<T>(ref T r, T v) where T : IComparable {
+			if (r.CompareTo(v) < 0) {
+				r = v;
+				return true;
+			} else {
+				return false;
+			}
+		}
+		public static bool ReplaceIfSmaller<T>(ref T r, T v) where T : IComparable {
+			if (0 < r.CompareTo(v)) {
+				r = v;
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		/// <summary>
+		/// 二分探索
+		/// ※条件を満たす最小のidxを返す
+		/// ※満たすものがない場合は ary.Length を返す
+		/// ※『aryの先頭側が条件を満たさない、末尾側が条件を満たす』という前提
+		/// ただし、IsOK(...)の戻り値を逆転させれば、逆でも同じことが可能。
+		/// </summary>
+		/// <param name="ary">探索対象配列 ★ソート済みであること</param>
+		/// <param name="key">探索値 ※これ以上の値を持つ（IsOKがtrueを返す）最小のindexを返す</param>
+		public static int BinarySearch<T>(T[] ary, T key) where T : IComparable, IComparable<T> {
+			int left = -1;
+			int right = ary.Length;
+
+			while (1 < right - left) {
+				var mid = left + (right - left) / 2;
+
+				if (IsOK(ary, mid, key)) {
+					right = mid;
+				} else {
+					left = mid;
+				}
+			}
+
+			// left は条件を満たさない最大の値、right は条件を満たす最小の値になっている
+			return right;
+		}
+		public static bool IsOK<T>(T[] ary, int idx, T key) where T : IComparable, IComparable<T> {
+			// key <= ary[idx] と同じ意味
+			return key.CompareTo(ary[idx]) <= 0;
+		}
+	}
+
+	public class SolverBase
+	{
+		virtual protected string ReadLine() => Console.ReadLine();
+		virtual protected string ReadString() => ReadLine();
+		virtual protected int ReadInt() => int.Parse(ReadLine());
+		//virtual protected void ReadInt2(out int x, out int y) {
+		//	var aryS = ReadLine().Split(' ');
+		//	x = int.Parse(aryS[0]);
+		//	y = int.Parse(aryS[1]);
+		//}
+		virtual protected long ReadLong() => long.Parse(ReadLine());
+		virtual protected string[] ReadStringArray() => ReadLine().Split(' ');
+		virtual protected int[] ReadIntArray() => ReadLine().Split(' ').Select<string, int>(s => int.Parse(s)).ToArray();
+		virtual protected void ReadInt2(out int a, out int b) {
+			var ary = ReadIntArray();
+			a = ary[0];
+			b = ary[1];
+		}
+		virtual protected void ReadInt3(out int a, out int b, out int c) {
+			var ary = ReadIntArray();
+			a = ary[0];
+			b = ary[1];
+			c = ary[2];
+		}
+		virtual protected long[] ReadLongArray() => ReadLine().Split(' ').Select<string, long>(s => long.Parse(s)).ToArray();
+		virtual protected double[] ReadDoubleArray() => ReadLine().Split(' ').Select<string, double>(s => double.Parse(s)).ToArray();
+		virtual protected void WriteLine(string line) => Console.WriteLine(line);
+		virtual protected void WriteLine(double d) => Console.WriteLine($"{d:F9}");
+		virtual protected void WriteLine<T>(T value) where T : IFormattable => Console.WriteLine(value);
+		virtual protected void WriteGrid(IEnumerable<IEnumerable<char>> arrayOfArray) {
+			var sb = new StringBuilder();
+			foreach (var a in arrayOfArray) {
+				foreach (var c in a) {
+					sb.Append(c);
+				}
+				sb.AppendLine();
+			}
+			WriteLine(sb.ToString());
+		}
+
+		[Conditional("DEBUG")]
+		protected void Dump(string s) => Console.WriteLine(s);
+		[Conditional("DEBUG")]
+		protected void Dump(char c) => Console.WriteLine(c);
+		[Conditional("DEBUG")]
+		protected void Dump(int x) => Console.WriteLine(x);
+		[Conditional("DEBUG")]
+		protected void Dump(double d) => Console.WriteLine($"{d:F9}");
+		[Conditional("DEBUG")]
+		protected void Dump<T>(IEnumerable<T> array) where T : IFormattable {
+			string s = Util.DumpToString(array);
+			// Consoleに出力すると、UnitTestの邪魔をしないというメリットあり。
+			Console.WriteLine(s);
+			//_writer.WriteLine(s);
+		}
+		[Conditional("DEBUG")]
+		protected void DumpGrid(IEnumerable<IEnumerable<char>> arrayOfArray) {
+			var sb = new StringBuilder();
+			foreach (var a in arrayOfArray) {
+				foreach (var c in a) {
+					sb.Append(c);
+				}
+				sb.AppendLine();
+			}
+			// Consoleに出力すると、UnitTestの邪魔をしないというメリットあり。
+			Console.WriteLine(sb.ToString());
+			//_writer.WriteLine(sb.ToString());
+		}
+		[Conditional("DEBUG")]
+		protected void DumpGrid<T>(IEnumerable<IEnumerable<T>> arrayOfArray) where T : IFormattable {
+			var sb = new StringBuilder();
+			foreach (var a in arrayOfArray) {
+				sb.AppendLine(Util.DumpToString(a));
+			}
+			// Consoleに出力すると、UnitTestの邪魔をしないというメリットあり。
+			Console.WriteLine(sb.ToString());
+			//_writer.WriteLine(sb.ToString());
+		}
+		[Conditional("DEBUG")]
+		protected void DumpDP<T>(T[,] dp) where T : IFormattable {
+			var sb = new StringBuilder();
+			for (int i = 0; i < dp.GetLength(0); i++) {
+				for (int j = 0; j < dp.GetLength(1); j++) {
+					sb.Append(dp[i, j]);
+					sb.Append(", ");
+				}
+				sb.AppendLine();
+			}
+			Console.WriteLine(sb.ToString());
+		}
+	}
+}
