db366fa7b404b222f1ab899478d1ead43866feeb
Leon <leopard10000@gmail.com>
2020-04-05 11:01:28 +0900

    ABC161-D: 約数列挙を改善してAC

--- AtCoderContests/ABC/ABC161/F.cs
+++ AtCoderContests/ABC/ABC161/F.cs
@@ -27,54 +27,26 @@ namespace ABC161.F
 
 			HashSet<long> set = new HashSet<long>();
 
-			List<long> primes = PrimeNumber.Factoring(N - 1).ToList();
-			// 全探索に使用する全bit数
-			var n = primes.Count;
-
-			// bit mask の生成
-			// 2^n     = 1 << n
-			for (ulong mask = 0; mask <= ((1ul << n) - 1); mask++) {
-				// N-1 の約数 e を全部求める
-				long e = 1;
-				// 各bitを下から舐める
-				for (int i = 0; i < n; i++) {
-
-					//iビット目が立っている？
-					if (0 != (mask & (1ul << i))) {
-						// 何か処理する
-						e *= primes[i];
-					}
-				}
-
-				if (N % e != 0) {
-					set.Add(e);
+			List<long> ds = ListDivisors(N - 1);
+			foreach (var d in ds) {
+				if (d == 1) continue;
+				if (N % d != 0) {
+					set.Add(d);
 				}
 			}
 
-			// Nの素因数
-			primes = PrimeNumber.Factoring(N).ToList();
-			n = primes.Count;
-			for (ulong mask = 0; mask <= ((1ul << n) - 1); mask++) {
-				// N の約数 e を全部求める
-				long e = 1;
-				// 各bitを下から舐める
-				for (int i = 0; i < n; i++) {
-
-					//iビット目が立っている？
-					if (0 != (mask & (1ul << i))) {
-						// 何か処理する
-						e *= primes[i];
-					}
+			ds = ListDivisors(N);
+			foreach (var d in ds) {
+				if (d == 1) continue;
+				var n = N;
+				while (n % d == 0) { 
+					n /= d;
 				}
-				if (e == 1) continue;
-				var d = N;
-				while (d % e == 0) { 
-					d /= e;
-				}
-				if (d % e == 1) {
-					set.Add(e);
+				if (n % d == 1) {
+					set.Add(d);
 				}
 			}
+
 			DumpEnumerable(set);
 			WriteLine(set.Count);
 		}
@@ -86,177 +58,11 @@ namespace ABC161.F
 #endif
 	}
 
-	public static class PrimeNumber
-	{
-		/// <summary>
-		/// 素数判定 【素朴な実装版】 ※拡張用に
-		/// </summary>
-		static bool IsPrimeNumber(long num) {
-			if (num < 2) return false;
-
-			for (long i = 2; i * i <= num; i++) {
-				if (num % i == 0) return false;
-			}
-			return true;
-		}
-
-
-		static long[] seedPrimes = {
-		      /*1,2,3,4, 5, 6, 7  8, 9,10,11,12,13,14,15,*/
-			2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97
-			};
-		/// <summary>
-		/// 素数判定 【高速版】
-		/// ★ System.Numerics.dll 必要 ※AtCoderは標準でloadされる
-		/// * using System.Numerics 必要
-		/// </summary>
-		public static bool IsPrime(long num) {
-			if (num == 1)
-				return false;
-			if (seedPrimes.Contains(num))
-				return true;
-			if (seedPrimes.Any(x => num % x == 0))
-				return false;
-
-			return (num < 2000000) ? IsPrimeBruteforce(num) : IsPrimeMillarRrabin(num);
-		}
-
-		/// <summary>
-		/// 素因数分解 ※1は返さない
-		/// </summary>
-		public static IEnumerable<long> Factoring(long n) {
-			while (1 < n) {
-				long factor = GetFactor(n);
-				yield return factor;
-				n /= factor;
-			}
-		}
-		private static long GetFactor(long n, int seed = 1) {
-			if (n % 2 == 0)
-				return 2;
-			if (IsPrime(n))
-				return n;
-			long x = 2;
-			long y = 2;
-			long d = 1;
-			long count = 0;
-			while (d == 1) {
-				count++;
-				x = f(x, n, seed);
-				y = f(f(y, n, seed), n, seed);
-				d = Gcd(Math.Abs(x - y), n);
-			}
-			if (d == n)
-				// 見つからなかった、乱数発生のシードを変えて再挑戦。
-				return GetFactor(n, seed + 1);
-			// 素数でない可能性もあるので、再度呼び出す
-			return GetFactor(d);
-		}
-		private static long f(long x, long n, int seed) {
-			return (seedPrimes[seed % 6] * x + seed) % n;
-		}
-		private static long Gcd(long a, long b) {
-			if (a < b)
-				return Gcd(b, a);  // 引数を入替えて自分を呼び出す
-			if (b == 0)
-				return a;
-			long d = 0;
-			do {
-				d = a % b;
-				a = b;
-				b = d;
-			} while (d != 0);
-			return a;
-		}
-
-		private static bool IsPrimeBruteforce(long num) {
-			if (num == 1)
-				return false;
-			if (num != 2 && num % 2 == 0)
-				return false;
-			if (num != 3 && num % 3 == 0)
-				return false;
-			if (num != 5 && num % 5 == 0)
-				return false;
-			long i = 0;
-			while (true) {
-				foreach (var p in seedPrimes.Skip(3).Take(8)) {
-					// 30m+2, 30m+3, 30m+4, 30m+5, 30m+6、30m+8、30m+9、30m+12... は割る必要はない。
-					var primeCandidte = p + i;
-					if (primeCandidte > Math.Sqrt(num))
-						return true;
-					if (num % (primeCandidte) == 0)
-						return false;
-				}
-				i += 30;
-			}
-		}
-
-		private static bool IsPrimeMillarRrabin(long num) {
-			if (num <= 1)
-				return false;
-			if ((num & 1) == 0)
-				return num == 2;
-
-			if (num < 100 && seedPrimes.Contains((int)num))
-				return true;
-
-			var WitnessMax = GetWitnessMax(num);
-
-			long d = num - 1;
-			long s = 0;
-			while ((d & 1) == 0) {
-				s++;
-				d >>= 1;
-			}
-			foreach (var w in seedPrimes.Take(WitnessMax)) {
-				if (!MillarRrabin(num, s, d, w))
-					return false;
-			}
-			return true;
-		}
-
-
-		private static int GetWitnessMax(long num) {
-			if (num < 2047)
-				return 1;
-			if (num < 1373653)
-				return 2;
-			if (num < 25326001)
-				return 3;
-			if (num < 3215031751)
-				return 4;
-			if (num < 2152302898747)
-				return 5;
-			if (num < 3474749660383)
-				return 6;
-			if (num < 341550071728321)
-				return 7;
-			if (num < 3825123056546413051)
-				return 9;
-			return 12;
-		}
-
-		private static bool MillarRrabin(long num, long s, long d, long witness) {
-			long x = ModPow(witness, d, num);
-			if (x == 1)
-				return true;
-			for (long r = 0; r < s; r++) {
-				if (x == num - 1)
-					return true;
-				BigInteger rem;
-				BigInteger.DivRem(BigInteger.Multiply(x, x), num, out rem);
-				x = (long)(rem);
-			}
-			return false;
-		}
-
-		private static long ModPow(long baseValue, long exponent, long modulus) {
-			return (long)BigInteger.ModPow(baseValue, exponent, modulus);
-		}
-	}
 	public static class Util
 	{
+		/// <summary>
+		/// 最大公約数
+		/// </summary>
 		public static int Gcd(int a, int b) {
 			if (a < b)
 				// 引数を入替えて自分を呼び出す
@@ -268,6 +74,21 @@ namespace ABC161.F
 			}
 			return a;
 		}
+		/// <summary>
+		/// n の約数のリストを返す ※ソートされていない
+		/// </summary>
+		public static List<long> ListDivisors(long n) {
+			List<long> ds = new List<long>();
+			for (long i = 1; i * i <= n; ++i) {
+				if (n % i != 0) continue;
+				ds.Add(i);
+
+				if (i * i != n)
+					ds.Add(n / i);
+			}
+			return ds;
+		}
+
 		public readonly static long MOD = 1000000007;
 
 		public static string JoinString<T>(IEnumerable<T> array) {
