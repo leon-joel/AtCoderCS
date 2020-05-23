28f03cdefda820235562b25e970c79697ee0c4e3
Leon <leopard10000@gmail.com>
2020-05-23 18:27:01 +0900

    第3回PASTリアルタイム受験: A-K をACして 70点（中級）
    ※普段のコンテストと比べて、より実装力を問われる問題が多かった。
    ※もう少し実装が速ければ L も解けた。
    ※K問題の実装がどこかに行ってしまった

---
+++ ContestsNetCore/PAST003/E_Test.cs
@@ -0,0 +1,176 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace PAST003.E
+{
+	public static class TestDataFactory
+	{
+		public static TestData[] Cases() {
+			return new TestData[] {
+				new TestData("Test1",
+@"3 2 3
+1 2
+2 3
+5 10 15
+1 2
+2 1 20
+1 1
+",
+@"10
+10
+20
+"),
+				new TestData("Test2",
+@"30 10 20
+11 13
+30 14
+6 4
+7 23
+30 8
+17 4
+6 23
+24 18
+26 25
+9 3
+18 4 36 46 28 16 34 19 37 23 25 7 24 16 17 41 24 38 6 29 10 33 38 25 47 8 13 8 42 40
+2 1 9
+1 8
+1 9
+2 20 24
+2 26 18
+1 20
+1 26
+2 24 31
+1 4
+2 21 27
+1 25
+1 29
+2 10 14
+2 2 19
+2 15 36
+2 28 6
+2 13 5
+1 12
+1 11
+2 14 43
+",
+@"18
+19
+37
+29
+8
+24
+18
+25
+46
+10
+18
+42
+23
+4
+17
+8
+24
+7
+25
+16
+"),
+//				new TestData("Test3",
+//@"",
+//@""),
+//				new TestData("Test4",
+//@"",
+//@""),
+//				new TestData("Test5",
+//@"",
+//@""),
+//				new TestData("Test6",
+//@"",
+//@""),
+//				new TestData("Test7",
+//@"4156000 10
+//",
+//@"0"),
+//				new TestData("Test8",
+//@"123498765000000 9
+//",
+//@"1"),
+//				new TestData("Test9",
+//@"262004 2
+//",
+//@"218"),
+
+//				new TestData("Test10",
+//@"2000000013
+//1",
+//@"1000000006"),
+//				new TestData("Test11",
+//@"2000000014
+//1",
+//@"0"),
+//				new TestData("Test12",
+//@"2000000015
+//1",
+//@"1"),
+			};
+		}
+	}
+
+	public class NunitSolver : Solver
+	{
+		readonly StringReader _reader;
+		readonly StringBuilder _sb;
+		public NunitSolver(string input, StringBuilder writer) {
+			_reader = new StringReader(input);
+			_sb = writer;
+		}
+		override protected string ReadLine() => _reader.ReadLine();
+		override protected string ReadString() => ReadLine();
+		override protected int ReadInt() => int.Parse(_reader.ReadLine());
+		override protected long ReadLong() => long.Parse(_reader.ReadLine());
+		override protected string[] ReadStringArray() => _reader.ReadLine().Split(' ');
+		override protected int[] ReadIntArray() => _reader.ReadLine().Split(' ').Select<string, int>(s => int.Parse(s)).ToArray();
+		override protected long[] ReadLongArray() => _reader.ReadLine().Split(' ').Select<string, long>(s => long.Parse(s)).ToArray();
+		override protected double[] ReadDoubleArray() => _reader.ReadLine().Split(' ').Select<string, double>(s => double.Parse(s)).ToArray();
+		override protected void WriteLine(string line) => _sb.AppendLine(line);
+		override protected void WriteLine(double d) => _sb.AppendLine($"{d:F9}");
+		override protected void WriteLine<T>(T value) => _sb.AppendLine(value.ToString());
+	}
+	[TestFixture()]
+	public class SolverTest
+	{
+		[TestCaseSource(typeof(TestDataFactory), "Cases")]
+		public void TestCase(TestData data) {
+			var sb = new StringBuilder();
+			var solver = new NunitSolver(data.Input, sb);
+			var sw = Stopwatch.StartNew();
+			solver.Run();
+			sw.Stop();
+			Console.WriteLine($"Elapsed: {sw.Elapsed}");
+			// 文字列比較
+			Assert.AreEqual(data.Expected.TrimEnd(), sb.ToString().TrimEnd());
+			// 浮動小数比較
+			//Assert.AreEqual(double.Parse(data.Expected), double.Parse(sb.ToString().TrimEnd()), 0.000000001);
+		}
+	}
+
+	public class TestData
+	{
+		public string Name { get; private set; }
+		public string Input { get; private set; }
+		public string Expected { get; private set; }
+
+		public override string ToString() => Name;
+
+		public TestData(string name, string input, string expected) {
+			Name = name;
+			Input = input;
+			Expected = expected.Replace("\r\n", "\n");
+		}
+	}
+}
