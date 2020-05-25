64ca5dfbce5ad777742411a21a1aae4beee890e2
Leon <leopard10000@gmail.com>
2020-05-25 21:59:29 +0900

    PAST002: D,E,F ※Fは PriorityQueue使用
    ※第3回PASTより難しく感じる。

---
+++ ContestsNetCore/PAST002/E_Test.cs
@@ -0,0 +1,122 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace PAST002.E
+{
+	public static class TestDataFactory
+	{
+		public static TestData[] Cases() {
+			return new TestData[] {
+				new TestData("Test1",
+@"6
+1 3 2 5 6 4
+",
+@"1 2 2 3 3 3
+"),
+				new TestData("Test2",
+@"3
+3 2 1
+",
+@"2 1 2
+"),
+				new TestData("Test3",
+@"2
+1 2
+",
+@"1 1"),
+//				new TestData("Test4",
+//@"",
+//@""),
+//				new TestData("Test5",
+//@"",
+//@""),
+//				new TestData("Test6",
+//@"",
+//@""),
+//				new TestData("Test7",
+//@"4156000 10
+//",
+//@"0"),
+//				new TestData("Test8",
+//@"123498765000000 9
+//",
+//@"1"),
+//				new TestData("Test9",
+//@"262004 2
+//",
+//@"218"),
+
+//				new TestData("Test10",
+//@"2000000013
+//1",
+//@"1000000006"),
+//				new TestData("Test11",
+//@"2000000014
+//1",
+//@"0"),
+//				new TestData("Test12",
+//@"2000000015
+//1",
+//@"1"),
+			};
+		}
+	}
+
+	public class NunitSolver : Solver
+	{
+		readonly StringReader _reader;
+		readonly StringBuilder _sb;
+		public NunitSolver(string input, StringBuilder writer) {
+			_reader = new StringReader(input);
+			_sb = writer;
+		}
+		override protected string ReadLine() => _reader.ReadLine();
+		override protected string ReadString() => ReadLine();
+		override protected int ReadInt() => int.Parse(_reader.ReadLine());
+		override protected long ReadLong() => long.Parse(_reader.ReadLine());
+		override protected string[] ReadStringArray() => _reader.ReadLine().Split(' ');
+		override protected int[] ReadIntArray() => _reader.ReadLine().Split(' ').Select<string, int>(s => int.Parse(s)).ToArray();
+		override protected long[] ReadLongArray() => _reader.ReadLine().Split(' ').Select<string, long>(s => long.Parse(s)).ToArray();
+		override protected double[] ReadDoubleArray() => _reader.ReadLine().Split(' ').Select<string, double>(s => double.Parse(s)).ToArray();
+		override protected void WriteLine(string line) => _sb.AppendLine(line);
+		override protected void WriteLine(double d) => _sb.AppendLine($"{d:F9}");
+		override protected void WriteLine<T>(T value) => _sb.AppendLine(value.ToString());
+	}
+	[TestFixture()]
+	public class SolverTest
+	{
+		[TestCaseSource(typeof(TestDataFactory), "Cases")]
+		public void TestCase(TestData data) {
+			var sb = new StringBuilder();
+			var solver = new NunitSolver(data.Input, sb);
+			var sw = Stopwatch.StartNew();
+			solver.Run();
+			sw.Stop();
+			Console.WriteLine($"Elapsed: {sw.Elapsed}");
+			// 文字列比較
+			Assert.AreEqual(data.Expected.TrimEnd(), sb.ToString().TrimEnd());
+			// 浮動小数比較
+			//Assert.AreEqual(double.Parse(data.Expected), double.Parse(sb.ToString().TrimEnd()), 0.000000001);
+		}
+	}
+
+	public class TestData
+	{
+		public string Name { get; private set; }
+		public string Input { get; private set; }
+		public string Expected { get; private set; }
+
+		public override string ToString() => Name;
+
+		public TestData(string name, string input, string expected) {
+			Name = name;
+			Input = input;
+			Expected = expected.Replace("\r\n", "\n");
+		}
+	}
+}
---
+++ ContestsNetCore/PAST002/F.cs
@@ -0,0 +1,476 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace PAST002.F
+{
+	using static Util;
+	using static Math;
+
+	public class Solver : SolverBase
+	{
+		public void Run() {
+			var N = ReadInt();
+
+			var bucket = new List<int>[N+1];
+			for (int i = 0; i < N+1; i++) {
+				bucket[i] = new List<int>();
+			}
+
+			for (int i = 0; i < N; i++) {
+				ReadInt2(out var A, out var B);
+				bucket[A].Add(B);
+			}
+
+			long ans = 0;
+
+			var q = new PriorityQueue<int>(N, true);
+			for (int i = 1; i <= N; i++) {
+				var tasks = bucket[i];
+
+				foreach (var t in tasks) {
+					q.Enqueue(t);
+				}
+
+				ans += q.Pop();
+				WriteLine(ans);
+			}
+		}
+
+#if !MYHOME
+		static void Main(string[] args) {
+			new Solver().Run();
+		}
+#endif
+	}
+	/// <summary>
+	/// 優先度付きキュー
+	/// </summary>
+	class PriorityQueue<T> where T : IComparable<T>
+	{
+		public T[] _heap;
+		public int _size;
+		public int _sign;
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		/// <param name="initialSize">Heap(List)の初期サイズ</param>
+		/// <param name="descend">降順の場合はtrueを与える</param>
+		public PriorityQueue(int initialSize, bool descend = false) {
+			_heap = new T[initialSize];
+			if (descend) _sign = -1;
+		}
+		public int Compare(T x, T y) {
+			return x.CompareTo(y) * _sign;
+		}
+		public void Push(T x) {
+			int i = _size++;
+			while (i > 0) {
+				int p = (i - 1) / 2;
+				if (Compare(x, _heap[p]) >= 0) {
+					break;
+				}
+				_heap[i] = _heap[p];
+				i = p;
+			}
+			_heap[i] = x;
+		}
+		public void Enqueue(T x) => Push(x);
+		/// <summary>
+		/// Countが0の場合はランタイムエラー
+		/// </summary>
+		public T Pop() {
+			T ret = _heap[0];
+			T x = _heap[--_size];
+			int i = 0;
+			while (i * 2 + 1 < _size) {
+				int a = i * 2 + 1;
+				int b = i * 2 + 2;
+				if (b < _size && Compare(_heap[a], _heap[b]) > 0) {
+					a = b;
+				}
+				if (Compare(_heap[a], x) >= 0) {
+					break;
+				}
+				_heap[i] = _heap[a];
+				i = a;
+			}
+			_heap[i] = x;
+			return ret;
+		}
+		public T Dequeue() => Pop();
+		/// <summary>
+		/// Countが0の場合は引数に与えた値を返す
+		/// </summary>
+		public T Pop(T defaultValue) {
+			if (Count() == 0)
+				return defaultValue;
+			else
+				return Pop();
+		}
+		public int Count() {
+			return _size;
+		}
+	}
+
+	public static class Util
+	{
+		/// <summary>反転した新しいstringを返す</summary>
+		public static string ReverseStr(this string s) {
+			return new string(s.Reverse().ToArray());
+		}
+
+		public static int Gcd(int a, int b) {
+			if (a < b)
+				// 引数を入替えて自分を呼び出す
+				return Gcd(b, a);
+			while (b != 0) {
+				var remainder = a % b;
+				a = b;
+				b = remainder;
+			}
+			return a;
+		}
+		public readonly static long MOD = 1000000007;
+
+		public static string JoinString<T>(IEnumerable<T> array) {
+			var sb = new StringBuilder();
+			foreach (var item in array) {
+				sb.Append(item);
+				sb.Append(", ");
+			}
+			return sb.ToString();
+		}
+
+		public static void InitArray<T>(T[] ary, T value) {
+			for (int i = 0; i < ary.Length; i++) {
+				ary[i] = value;
+			}
+		}
+		public static void InitDP2<T>(T[,] dp, T value) {
+			for (int i = 0; i < dp.GetLength(0); i++) {
+				for (int j = 0; j < dp.GetLength(1); j++) {
+					dp[i, j] = value;
+				}
+			}
+		}
+		public static void InitDP3<T>(T[,,] dp, T value) {
+			for (int i = 0; i < dp.GetLength(0); i++) {
+				for (int j = 0; j < dp.GetLength(1); j++) {
+					for (int k = 0; k < dp.GetLength(2); k++) {
+						dp[i, j, k] = value;
+					}
+				}
+			}
+		}
+
+		/// <summary>charでも対応可能なMax</summary>
+		public static T Max<T>(T a, T b) where T : IComparable {
+			return 0 <= a.CompareTo(b) ? a : b;
+		}
+		/// <summary>3要素以上に対応するMax</summary>
+		public static T Max<T>(params T[] nums) where T : IComparable {
+			if (nums.Length == 0) return default(T);
+
+			T max = nums[0];
+			for (int i = 1; i < nums.Length; i++) {
+				max = max.CompareTo(nums[i]) > 0 ? max : nums[i];
+			}
+			return max;
+		}
+		/// <summary>charでも対応可能なMin</summary>
+		public static T Min<T>(T a, T b) where T : IComparable {
+			return 0 < a.CompareTo(b) ? b : a;
+		}
+		/// <summary>3要素以上に対応するMin</summary>
+		public static T Min<T>(params T[] nums) where T : IComparable {
+			if (nums.Length == 0) return default(T);
+
+			T min = nums[0];
+			for (int i = 1; i < nums.Length; i++) {
+				min = min.CompareTo(nums[i]) < 0 ? min : nums[i];
+			}
+			return min;
+		}
+
+		///<summary>targetValueに一番近い値を返す</summary>
+		public static long Nearest(long targetValue, params long[] values) {
+			Debug.Assert(0 < values.Length);
+			long minDiff = long.MaxValue;
+			long ans = long.MaxValue;
+			foreach (var v in values) {
+				var diff = Math.Abs(v - targetValue);
+				if (ReplaceIfSmaller(ref minDiff, diff)) {
+					ans = v;
+				}
+			}
+			return ans;
+		}
+
+		/// <summary>
+		/// ソート済み配列 ary に同じ値の要素が含まれている？
+		/// ※ソート順は昇順/降順どちらでもよい
+		/// </summary>
+		public static bool HasDuplicateInSortedArray<T>(T[] ary) where T : IComparable, IComparable<T> {
+			if (ary.Length <= 1) return false;
+
+			var lastNum = ary[ary.Length - 1];
+
+			foreach (var n in ary) {
+				if (lastNum.CompareTo(n) == 0) {
+					return true;
+				} else {
+					lastNum = n;
+				}
+			}
+			return false;
+		}
+
+		///<summary>v が r より大きい場合、r に v を代入し、trueを返す。それ以外（同値の場合を含む）は何もせずfalseを返す</summary>
+		public static bool ReplaceIfBigger<T>(ref T r, T v) where T : IComparable {
+			if (r.CompareTo(v) < 0) {
+				r = v;
+				return true;
+			} else {
+				return false;
+			}
+		}
+		///<summary>v が r よりが小さい場合、r に v を代入し、trueを返す。それ以外（同値の場合を含む）は何もせずfalseを返す</summary>
+		public static bool ReplaceIfSmaller<T>(ref T r, T v) where T : IComparable {
+			if (0 < r.CompareTo(v)) {
+				r = v;
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		public static void Swap<T>(ref T a, ref T b) where T : class {
+			var tmp = a;
+			a = b;
+			b = tmp;
+		}
+
+		/// <summary>
+		/// dic[key]にadderを加算する。keyが存在しなかった場合はdic[key]=adder をセットする。
+		/// </summary>
+		public static void AddTo<TKey>(this Dictionary<TKey, int> dic, TKey key, int adder) {
+			if (dic.ContainsKey(key)) {
+				dic[key] += adder;
+			} else {
+				dic[key] = adder;
+			}
+		}
+
+		/// <summary>
+		/// 文字列 s が chars に含まれる文字を含んでいるか？
+		/// </summary>
+		public static bool ContainsAny(this string s, char[] chars) {
+			for (int j = 0; j < s.Length; j++) {
+				if (chars.Contains(s[j])) return true;
+			}
+			return false;
+		}
+
+		/// <summary>
+		/// 二分探索
+		/// ※条件を満たす最小のidxを返す
+		/// ※満たすものがない場合は ary.Length を返す
+		/// ※『aryの先頭側が条件を満たさない、末尾側が条件を満たす』という前提
+		/// ただし、IsOK(...)の戻り値を逆転させれば、逆でも同じことが可能。
+		/// </summary>
+		/// <param name="ary">探索対象配列 ★ソート済みであること</param>
+		/// <param name="key">探索値 ※これ以上の値を持つ（IsOKがtrueを返す）最小のindexを返す</param>
+		public static int BinarySearch<T>(T[] ary, T key) where T : IComparable, IComparable<T> {
+			int left = -1;
+			int right = ary.Length;
+
+			while (1 < right - left) {
+				var mid = left + (right - left) / 2;
+
+				if (IsOK(ary, mid, key)) {
+					right = mid;
+				} else {
+					left = mid;
+				}
+			}
+
+			// left は条件を満たさない最大の値、right は条件を満たす最小の値になっている
+			return right;
+		}
+		public static bool IsOK<T>(T[] ary, int idx, T key) where T : IComparable, IComparable<T> {
+			// key <= ary[idx] と同じ意味
+			return key.CompareTo(ary[idx]) <= 0;
+		}
+
+		/// <summary>
+		/// nの 2進数における下からd(0-indexed)桁目のbitが立っている？
+		/// </summary>
+		public static int GetBit(long n, int d) {
+			if (0 == (n & (1L << d)))
+				return 0;
+			else
+				return 1;
+		}
+	}
+
+	public class SolverBase
+	{
+		virtual protected string ReadLine() => Console.ReadLine();
+		virtual protected string ReadString() => ReadLine();
+		virtual protected int ReadInt() => int.Parse(ReadLine());
+		virtual protected long ReadLong() => long.Parse(ReadLine());
+		virtual protected string[] ReadStringArray() => ReadLine().Split(' ');
+		virtual protected void ReadString2(out string a, out string b) {
+			var ary = ReadStringArray();
+			a = ary[0];
+			b = ary[1];
+		}
+		virtual protected void ReadString3(out string a, out string b, out string c) {
+			var ary = ReadStringArray();
+			a = ary[0];
+			b = ary[1];
+			c = ary[2];
+		}
+		virtual protected char[] ReadCharArray() => ReadLine().Split(' ').Select<string, char>(s => s[0]).ToArray();
+		virtual protected int[] ReadIntArray() => ReadLine().Split(' ').Select<string, int>(s => int.Parse(s)).ToArray();
+		virtual protected void ReadInt2(out int a, out int b) {
+			var ary = ReadIntArray();
+			a = ary[0];
+			b = ary[1];
+		}
+		virtual protected void ReadInt3(out int a, out int b, out int c) {
+			var ary = ReadIntArray();
+			a = ary[0];
+			b = ary[1];
+			c = ary[2];
+		}
+		virtual protected void ReadInt4(out int a, out int b, out int c, out int d) {
+			var ary = ReadIntArray();
+			a = ary[0];
+			b = ary[1];
+			c = ary[2];
+			d = ary[3];
+		}
+		virtual protected long[] ReadLongArray() => ReadLine().Split(' ').Select<string, long>(s => long.Parse(s)).ToArray();
+		virtual protected void ReadLong2(out long a, out long b) {
+			var ary = ReadLongArray();
+			a = ary[0];
+			b = ary[1];
+		}
+		virtual protected void ReadLong3(out long a, out long b, out long c) {
+			var ary = ReadLongArray();
+			a = ary[0];
+			b = ary[1];
+			c = ary[2];
+		}
+		virtual protected void ReadLong4(out long a, out long b, out long c, out long d) {
+			var ary = ReadLongArray();
+			a = ary[0];
+			b = ary[1];
+			c = ary[2];
+			d = ary[3];
+		}
+		virtual protected double[] ReadDoubleArray() => ReadLine().Split(' ').Select<string, double>(s => double.Parse(s)).ToArray();
+
+		virtual protected void WriteLine(string line) => Console.WriteLine(line);
+		virtual protected void WriteLine(double d) => Console.WriteLine($"{d:F9}");
+		virtual protected void WriteLine<T>(T value) where T : IFormattable => Console.WriteLine(value);
+		virtual protected void WriteGrid(IEnumerable<IEnumerable<char>> arrayOfArray) {
+			var sb = new StringBuilder();
+			foreach (var a in arrayOfArray) {
+				foreach (var c in a) {
+					sb.Append(c);
+				}
+				sb.AppendLine();
+			}
+			WriteLine(sb.ToString());
+		}
+
+		[Conditional("DEBUG")]
+		protected void Dump(double d) => Console.WriteLine($"{d:F9}");
+		[Conditional("DEBUG")]
+		protected void Dump<T>(T x) => Console.WriteLine(x);
+		[Conditional("DEBUG")]
+		protected void DumpArray<T>(IEnumerable<T> array) {
+			string s = Util.JoinString(array);
+			// Consoleに出力すると、UnitTestの邪魔をしないというメリットあり。
+			Console.WriteLine(s);
+			//_writer.WriteLine(s);
+		}
+		[Conditional("DEBUG")]
+		protected void DumpGrid(IEnumerable<IEnumerable<char>> arrayOfArray) {
+			var sb = new StringBuilder();
+			foreach (var a in arrayOfArray) {
+				foreach (var c in a) {
+					sb.Append(c);
+				}
+				sb.AppendLine();
+			}
+			// Consoleに出力すると、UnitTestの邪魔をしないというメリットあり。
+			Console.WriteLine(sb.ToString());
+			//_writer.WriteLine(sb.ToString());
+		}
+		[Conditional("DEBUG")]
+		protected void DumpGrid<T>(IEnumerable<IEnumerable<T>> arrayOfArray) {
+			var sb = new StringBuilder();
+			foreach (var a in arrayOfArray) {
+				sb.AppendLine(Util.JoinString(a));
+			}
+			// Consoleに出力すると、UnitTestの邪魔をしないというメリットあり。
+			Console.WriteLine(sb.ToString());
+			//_writer.WriteLine(sb.ToString());
+		}
+		[Conditional("DEBUG")]
+		protected void DumpGrid(bool[,] grid) {
+			var sb = new StringBuilder();
+			for (int i = 0; i < grid.GetLength(0); i++) {
+				for (int j = 0; j < grid.GetLength(1); j++) {
+					sb.Append(grid[i, j] ? "x " : ". ");
+				}
+				sb.AppendLine();
+			}
+			Console.WriteLine(sb.ToString());
+		}
+		[Conditional("DEBUG")]
+		protected void DumpGrid(char[,] grid) {
+			var sb = new StringBuilder();
+			for (int i = 0; i < grid.GetLength(0); i++) {
+				for (int j = 0; j < grid.GetLength(1); j++) {
+					sb.Append(grid[i, j]);
+					sb.Append(" ");
+				}
+				sb.AppendLine();
+			}
+			Console.WriteLine(sb.ToString());
+		}
+		[Conditional("DEBUG")]
+		protected void DumpDP<T>(T[,] dp) where T : IFormattable {
+			var sb = new StringBuilder();
+			for (int i = 0; i < dp.GetLength(0); i++) {
+				for (int j = 0; j < dp.GetLength(1); j++) {
+					sb.Append(dp[i, j]);
+					sb.Append(", ");
+				}
+				sb.AppendLine();
+			}
+			Console.WriteLine(sb.ToString());
+		}
+		[Conditional("DEBUG")]
+		protected void DumpDP3_Keta<T>(T[,,] dp) where T : IFormattable {
+			var sb = new StringBuilder();
+			for (int i = 0; i < dp.GetLength(0); i++) {
+				sb.Append($"{i,2}: ");
+				for (int j = 0; j < dp.GetLength(1); j++) {
+					sb.Append($"{dp[i, j, 0]}-{dp[i, j, 1]}");
+					sb.Append(", ");
+				}
+				sb.AppendLine();
+			}
+			Console.WriteLine(sb.ToString());
+		}
+	}
+}
